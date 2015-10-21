c7763e6f8f76d7010e37d9a03632ef7e87d12aa3
TGuillerme <guillert@tcd.ie>
2015-10-21 14:47:50 +0100

    Updated test.dispRity to work with two functions
    Need to develop more than three series comparisons now!

--- R/dispRity.test.R
+++ R/dispRity.test.R
@@ -30,5 +30,64 @@
 # '
 # ' @author Thomas Guillerme
 
+
+
+
+
 #Calculating each axis variance
 dispRity.tests <- function(X) return(X)
+
+
+Anderson.test<-function(BSresults, time_pco) {
+    #Disparity T-Test calculation from Anderson and Friedman 2012.
+    #Code modified from Smith et al. 2014 - Evolution
+
+    #Extracting the sample sizes
+    sample_size<-unlist(lapply(time_pco, nrow))
+
+    #Getting the mean and the variance from the BSresults
+    variance_int<-apply(BSresults, 2, var)
+    mean_int<-apply(BSresults, 2, mean)
+
+    #Calculating the T_statistics functions
+    mean.difference<-function(x,y, mean_int) {mean_int[x]-mean_int[y]}
+    term.A<-function(x,y,sample_size, variance_int) { ((sample_size[x]-1)*(sample_size[x])* variance_int[x] + (sample_size[y]-1)*(sample_size[y])* variance_int[y] )/(sample_size[x]+sample_size[y]+2) }
+    term.B<-function(x,y, sample_size) { (sample_size[x] + sample_size [y])/(sample_size[x] * sample_size [y]) }
+
+    #Calculating the statistic, df and p-value.
+    difference<-p_values<-degrees_freedom<-t_statistic<-as.data.frame(matrix(NA, nrow=ncol(BSresults), ncol=ncol(BSresults)))
+    rownames(difference)<-rownames(p_values)<-rownames(degrees_freedom)<-rownames(t_statistic)<-names(time_pco)
+    colnames(difference)<-colnames(p_values)<-colnames(degrees_freedom)<-colnames(t_statistic)<-names(time_pco)
+    for(row in 1:ncol(BSresults)) {
+        for(col in 1:ncol(BSresults)) {
+            #Calculate difference
+            difference[row,col]<-mean.difference(row,col, mean_int)
+            #Calculate T
+            t_statistic[row,col]<-mean.difference(row,col, mean_int)/sqrt(term.A(row,col,sample_size,variance_int)*term.B(row,col,sample_size))
+            if(!is.finite(t_statistic[row,col])) {
+                #Exist the loop if some variance or differences are not finit numbers.
+                message("T statistic cannot be calculate. Probable reason: some values are is near Inf or -Inf.")
+                return(list("diff"=difference, "df"=degrees_freedom, "T"=t_statistic, "p"=p_values))
+            }
+
+            #Calculate df
+            degrees_freedom[row,col]<-sample_size[row]+sample_size[col]-2
+            #Calculate p
+            p_values[row,col]<- 1-pt(t_statistic[row, col], df = degrees_freedom[row, col])
+
+            #make test two-tailed
+            if (p_values [row,col] > 0.5) {
+                p_values [row,col] <- 2*(1-p_values[row,col])
+            } else {
+                if (p_values [row,col] < 0.5){
+                    p_values [row,col] <- 2*(p_values[row,col])   
+                } else {
+                    if (p_values [row,col] == 0.5){
+                        p_values [row,col] <- 1
+                    }
+                }
+            }
+        }
+    }
+    return(list("diff"=difference, "df"=degrees_freedom, "T"=t_statistic, "p"=p_values))
+}
--- R/test.dispRity.R
+++ R/test.dispRity.R
@@ -1,39 +1,53 @@
-# ' @title Disparity statistics 
-# '
-# ' @description Applying statistical tests to dispRity objects
-# '
-# ' @param data A \code{dispRity} object.
-# ' @param test A statistical \code{function} to apply to the data.
-# ' @param format The expected input format to the function (either \code{"matrix"} or \code{"vector"} - default).
-# ' @param ... Additional options to pass to the test \code{function}.
-# '
-# ' @examples
-# ' ## Load the Beck & Lee 2014 data
-# ' data(BeckLee_mat50)
-# '
-# ' ## Calculating the disparity from a customised series
-# ' ## Generating the series
-# ' factors <- as.data.frame(matrix(data = c(rep(1, nrow(BeckLee_mat50)/2),
-# '      rep(2, nrow(BeckLee_mat50)/2)), nrow = nrow(BeckLee_mat50), ncol = 1,
-# '      dimnames = list(rownames(BeckLee_mat50))))
-# ' customised_series <- cust.series(BeckLee_mat50, factors)
-# ' ## Bootstrapping the data
-# ' bootstrapped_data <- boot.matrix(customised_series, bootstraps=100)
-# ' ## Caculating the sum of ranges
-# ' sum_of_ranges <- dispRity(bootstrapped_data, metric=c(sum, ranges))
-# '
-# ' ## Summarising the results
-# ' summary(sum_of_ranges) # default
-# ' ## Using different options
-# ' summary(sum_of_ranges, quantile = 75, cent.tend = median,
-# '      rounding=  0, recall = TRUE)
-# ' 
-# ' @seealso \code{\link{dispRity}}
-# '
-# ' @author Thomas Guillerme
+#' @title Disparity statistics 
+#'
+#' @description Applying statistical tests to dispRity objects
+#'
+#' @param data A \code{dispRity} object.
+#' @param test A statistical \code{function} to apply to the data.
+##' @param format The expected input format to the function (either \code{"matrix"} or \code{"vector"} - default).
+#' @param comparisons If data contains more than two series, the type of comparisons to apply: either \code{"referential"}, \code{"sequential"}, \code{"pairwise"} or an even \code{vector} of series names/number to compare (see details).
+#' @param ... Additional options to pass to the test \code{function}.
+#'
+#' @details  
+#' The \code{comparison} argument can be:
+#' \itemize{
+#'   \item \code{"referential"}: compares the first series to all the others.
+#'   \item \code{"sequential"}: compares each series sequentially (e.g. first against second, second against third, etc.).
+#'   \item \code{"pairwise"}: pairwise comparisons of all the series.
+#'   \item An even \code{vector} of names or number of series to compare.
+#' }
+#'
+#' @examples
+#' ## Load the Beck & Lee 2014 data
+#' data(BeckLee_mat50)
+#'
+#' ## Calculating the disparity from a customised series
+#' ## Generating the series
+#' factors <- as.data.frame(matrix(data = c(rep(1, nrow(BeckLee_mat50)/2),
+#'      rep(2, nrow(BeckLee_mat50)/2)), nrow = nrow(BeckLee_mat50), ncol = 1,
+#'      dimnames = list(rownames(BeckLee_mat50))))
+#' customised_series <- cust.series(BeckLee_mat50, factors)
+#' ## Bootstrapping the data
+#' bootstrapped_data <- boot.matrix(customised_series, bootstraps=100)
+#' ## Caculating the sum of ranges
+#' sum_of_ranges <- dispRity(bootstrapped_data, metric=c(sum, ranges))
+#'
+#' ## Summarising the results
+#' summary(sum_of_ranges) # default
+#' ## Using different options
+#' summary(sum_of_ranges, quantile = 75, cent.tend = median,
+#'      rounding=  0, recall = TRUE)
+#' 
+#' @seealso \code{\link{dispRity}}
+#'
+#' @author Thomas Guillerme
 
 
-test.dispRity<-function(data, test, format, ...) {
+test.dispRity<-function(data, test, comparisons, ...) { #format
+
+    #get call
+    match_call<-match.call()
+
     #----------------------
     # SANITIZING
     #----------------------
@@ -60,123 +74,69 @@ test.dispRity<-function(data, test, format, ...) {
         is.bootstrapped<-FALSE
     }
     
-    #quantile
-    #Only check if the data is bootstrapped
-    if(is.bootstrapped == TRUE) {
-        check.class(quantile, "numeric", " must be any value between 1 and 100.")
-        #remove warnings
-        options(warn=-1)
-        if(any(quantile) < 1) {
-            stop("Quantile(s) must be any value between 1 and 100.")
-        }
-        if(any(quantile) > 100) {
-            stop("Quantile(s) must be any value between 1 and 100.")
-        }
-        options(warn=0)
-    }
+    #Test
+    #must be a single function
+    check.class(test, "function", " must be a single function.")
+    check.length(test, 1, " must be a single function.")
 
-    #cent.tend
-    #Must be a function
-    check.class(cent.tend, "function")
-    #The function must work
-    silent<-check.metric(cent.tend)
+    # #Format
+    # #must be a single character string
+    # check.class(format, "character", " must be either 'matrix' or 'vector'.")
+    # check.length(format, 1, " must be either 'matrix' or 'vector'.")
+    # #must be either matrix or vector
+    # if(format != "matrix") {
+    #     if(format != "vector") {
+    #         stop(paste(as.expression(match_call$format), " must be either 'matrix' or 'vector'.", sep=""))
+    #     }
+    # }
 
-    #recall
-    check.class(recall, "logical")
-
-    #rounding
-    if(missing(rounding)) {
-        #Set to default (see below)
-        rounding<-"default"
-    } else {
-        check.class(rounding, "numeric")
-    }
-
-    #Get call
-    match_call<-match.call()
-
-    #----------------------
-    # TRANSFORMING THE DATA INTO A TABLE
-    #----------------------
-
-    if(is.bootstrapped == FALSE) {
-        #Extracting the observed disparity
-        OBSresults_unl<-unlist(unlist(recursive.unlist(OBSresults), recursive=FALSE))
-
-        #Create the result table
-        results_table<-data.frame(cbind(rep(data$series, unlist(lapply(OBSresults_unl, length))), diversity.count(data$data$observed), as.numeric(OBSresults_unl)), stringsAsFactors=FALSE)
-
-        #Add columns names
-        colnames(results_table)<-c("series", "n", "observed")
-    
-    } else {
-        #Extracting the observed disparity
-        OBSresults_unl<-unlist(unlist(recursive.unlist(OBSresults), recursive=FALSE))
-
-        #Unlisting the bootstrap values
-        BSresults_unl<-unlist(recursive.unlist(BSresults), recursive=FALSE)
-        
-        #Calculating the central tendency
-        results_cent<-unlist(lapply(BSresults_unl, cent.tend))
-
-        #Multiplier (for rep)
-        multiplier<-unlist(lapply(BSresults, length))
-
-        #Create the result table
-        results_table<-data.frame(cbind(rep(data$series, multiplier), diversity.count(data$data$bootstraps), rep(OBSresults_unl, multiplier), results_cent), stringsAsFactors=FALSE)
-
-        #Add columns names
-        if(is.null(match_call$cent.tend)) {
-            colnames(results_table)<-c("series", "n", "observed", "mean")
+    #Comparisons
+    test_data_length <- extract.dispRity(data)
+    #Stop if only one series
+    if(length(test_data_length) == 1) stop(paste(as.expression(match_call$data), " must have at least two series.", sep=""))
+    #Ignore if length data = 2
+    if(length(test_data_length) == 2) comparisons <- NULL
+    #Else, check comparisons
+    if(length(test_data_length) > 2) {
+        is.pair <- FALSE
+        all_comparisons <- c("referential", "sequential", "pairwise")
+        if(all(is.na(match(comparisons, all_comparisons)))) {
+            #Check if the length of comparisons is even
+            if(comparisons%%2 != 0) stop(paste(as.expression(match_call$comparisons), " is not an even vector!", sep=""))
+            #Check if the given vector corresponds to the series names
+            if(class(comparisons) == "character") {
+                if(any(is.na(match(comparisons, data$series)))) stop(paste(as.expression(match_call$comparisons), ": at least one series was not found.", sep=""))
+            }
+            #Check if the given vector corresponds to the series numbers
+            if(class(comparisons) == "numeric") {
+                if(any(is.na(match(comparisons, seq(1:length(data$series)))))) stop(paste(as.expression(match_call$comparisons), ": at least one series was not found.", sep=""))
+            }
         } else {
-            colnames(results_table)<-c("series", "n", "observed", match_call$cent.tend)
-        }
-
-        #Checking if the observed values match the n_obs (otherwise replace by NA)
-        n_obs<-diversity.count(data$data$observed)
-        for(ser in 1:length(data$series)) {
-            to_remove<-which(as.numeric(results_table[which(results_table$series == data$series[ser]), 2]) != n_obs[ser])
-            results_table[which(results_table$series == data$series[ser]), 3][to_remove] <- NA          
-        }
-
-        #Calculate the quantiles
-        results_quantile<-lapply(BSresults_unl, quantile, probs=CI.converter(quantile))
-
-        #Add to the result table
-        results_table<-cbind(results_table, matrix(data=unlist(results_quantile), ncol=length(quantile)*2, byrow=TRUE))
-
-        #Add the quantile names
-        colnames(results_table)[c(5:(length(quantile)*2+4))]<-names(results_quantile[[1]])
-    }
-
-    #Round the results (number of decimals = maximum number of digits in the entire)
-    if(rounding == "default") {
-        for(column in 3:ncol(results_table)) {
-            if(class(results_table[,column]) != "factor") {
-                results_table[,column]<-round(as.numeric(results_table[,column]), digit=get.digit(as.numeric(results_table[,column])))
-            } else {
-                results_table[,column]<-round(as.numeric(as.character(results_table[,column])), digit=get.digit(as.numeric(as.character(results_table[,column]))))
-            }
+            #Make sure only one inbuilt comparison is given
+            check.length(comparisons, 1, " must be either 'referential', 'sequential', 'pairwise' or a vector of series names/numbers.")
         }
     } else {
-        for(column in 3:ncol(results_table)) {
-            if(class(results_table[,column]) != "factor") {
-                results_table[,column]<-round(as.numeric(results_table[,column]), digit=rounding)
-            } else {
-                results_table[,column]<-round(as.numeric(as.character(results_table[,column])), digit=rounding)
-            }
-        }
+        is.pair <- TRUE
     }
 
-    #Make the rarefaction column numeric
-    results_table[,2]<-as.numeric(results_table[,2])
+    #----------------------
+    # APPLYING THE TEST
+    #----------------------
+    #~~~~~~~~~~
+    # Add a controller for observed data
+    #~~~~~~~~~~
 
-    #----------------------
-    # OUTPUT
-    #----------------------
-    if(recall==TRUE) {
-        cat(data$call, sep="\n")
+    #Extracting the data
+    extracted_data <- extract.dispRity(data, observed=FALSE)
+
+    #Apply the simple test if data is a pair
+    if(is.pair == TRUE) {
+        #running the test
+        output <- test(extracted_data[[1]], extracted_data[[2]])
+        #fixing the data name (if hclass)
+        if(class(output) == "hclass") output$data.name <- paste(data$series, collapse=" and ")
     }
-    return(results_table)
 
+    #output
+    return(output)
 }
