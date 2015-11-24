c846a90dcba605c4b3992210b4e5c635885b674f
TGuillerme <guillert@tcd.ie>
2015-11-24 17:04:07 +0000

    Added volume + metric testing

--- R/dispRity.metric.R
+++ R/dispRity.metric.R
@@ -10,10 +10,17 @@
 #' level1.fun(X)
 #'
 #' @param matrix A matrix.
-#' @param X A vector or a matrix.
+#' @param X A vector.
 #'
 #' @details
 #' These are inbuilt functions for calculating disparity. See \code{\link{make.metric}} for details on \code{level3.fun}, \code{level2.fun} and \code{level1.fun}.
+#' The currently implemented matrix aggregate metrics (\code{level3.fun}) are:
+#' \itemize{
+#'   \item \code{hyper.volume}: calculates the hyperdimensional volume of a matrix.
+#'      \itemize{
+#'          \item WARNING: this function only calculates the exact volume from MDS or PCO (PCoA) ordinations (e.g. \code{\link[stats]{cmdscale}}, \code{\link[ape]{pcoa}})
+#'      }
+#' }
 #' The currently implemented vector aggregate metrics (\code{level2.fun}) are:
 #' \itemize{
 #'   \item \code{ranges}: calculates the range of each axis of the matrix.
@@ -23,7 +30,6 @@
 #' The currently implemented value aggregate metrics (\code{level1.fun}) are:
 #' \itemize{
 #'   \item \code{mode.val}: calculates the modal value of a vector.
-#'   \item \code{volume}: calculates the volume of a matrix.
 #' }
 #' See also \code{\link[base]{mean}}, \code{\link[stats]{median}}, \code{\link[base]{sum}} or \code{\link[base]{prod}} for commonly used summary metrics.
 #' 
@@ -36,29 +42,28 @@
 #' @author Thomas Guillerme
 
 #Calculating each axis variance
-variances<-function(matrix) {
+variances <- function(matrix) {
     return(apply(matrix, 2, var))
 }
 
 #Calculating each axis ranges
-ranges<-function(matrix) {
+ranges <- function(matrix) {
     #Max values
-    max_values<-apply(matrix, 2, max)
+    max_values <- apply(matrix, 2, max)
     #Min values
-    min_values<-apply(matrix, 2, min)
+    min_values <- apply(matrix, 2, min)
     #Ranges values
-    ranges<-abs(max_values-min_values)
+    ranges <- abs(max_values-min_values)
     return(ranges)
 }
 
 #Calculating the distance from centroid
-centroids<-function(matrix) {
-
+centroids <- function(matrix) {
     #Calculating the centroid point
-    centroid<-apply(matrix, 2, mean)
+    centroid <- apply(matrix, 2, mean)
 
     #Calculating the distance from centroid
-    cent.dist<-NULL
+    cent.dist <- NULL
     for (j in 1:nrow(matrix)){
         cent.dist[j] <- dist(rbind(matrix[j,], centroid), method="euclidean")
     }
@@ -66,9 +71,19 @@ centroids<-function(matrix) {
     return(cent.dist)
 }
 
-# Summary functions:
-# These functions should take a vector as an input and output a single numeric character
-
-mode.val<-function(X){
+# Calculate the mode of a vector
+mode.val <- function(X){
     return(as.numeric(names(sort(-table(X))[1])))
+}
+
+# Calculate the volume of an eigen matrix (modified from Donohue et al 2013, Ecology Letters)
+hyper.volume <- function(matrix) {
+    # The eigen value is equal to the sum of the variance/covariance within each axis
+    # multiplied by the maximum number of dimensions (k-1) - ONLY WORKS FOR MDS OR PCO!
+    eigen.value<-abs(apply(var(matrix),2, sum)*(nrow(matrix)-1))
+
+    #volume (from Donohue et al 2013, Ecology Letters)
+    volume<-pi^(ncol(matrix)/2)/gamma((ncol(matrix)/2)+1)*prod(eigen.value^(0.5))
+
+    return(volume)
 }
---
+++ tests/testthat/test-dispRity.metric.R
@@ -0,0 +1,156 @@
+#TESTING boot.matrix
+
+context("dispRity.metric")
+
+
+#Testing the metrics
+test_that("variances metric", {
+    #Create a dummy matrix
+    matrix <- replicate(50, rnorm(100))
+    #Calculate the variances
+    vars1 <- variances(matrix)
+    expect_equal(length(vars1), ncol(matrix))
+    #Calculate the variances "manually"
+    vars2 <- apply(matrix, 2, var)
+    #test
+    expect_equal(vars1, vars2)
+})
+
+test_that("ranges metric", {
+    #Create a dummy matrix
+    matrix <- replicate(50, rnorm(100))
+    #Calculate the ranges
+    ran1 <- ranges(matrix)
+    expect_equal(length(ran1), ncol(matrix))
+    #Calculate the variances "manually"
+    ran2 <- apply(matrix, 2, function(X) abs(min(X)-max(X)))
+    #test
+    expect_equal(ran1, ran2)
+})
+
+test_that("centroids metric", {
+    #Create a dummy matrix
+    matrix <- replicate(50, rnorm(100))
+    #Calculate the centroids
+    cent1 <- centroids(matrix)
+    expect_equal(length(cent1), nrow(matrix))
+    #Calculate the centroids "manually"
+    centroid <- apply(matrix, 2, mean)
+    cent2 <- NULL
+    for (j in 1:nrow(matrix)){
+        cent2[j] <- dist(rbind(matrix[j,], centroid), method="euclidean")
+    }
+    #test
+    expect_equal(cent1, cent2)
+})
+
+test_that("mode.val metric", {
+    #Create a dummy vector
+    vector <- rnorm(100)
+    #Calculate the mode
+    mode1 <- mode.val(vector)
+    #Calculate the variances "manually"
+    mode2 <- as.numeric(names(sort(-table(vector))[1]))
+    #test
+    expect_equal(mode1, mode2)
+})
+
+test_that("volume metric", {
+    # Calculate the proper volume (using the eigen values)
+    volume.true <- function(matrix, eigen.val) {
+        #Correct calculation of the volume (using the eigen values)
+        #remove the eigen values for the eigen vectors not present in matrix
+        eigen.val<-eigen.val[1:ncol(matrix)]
+
+        #dimensionality (where k (or n in Donohue et al 2013) is the size of the covariance matrix but if corrected is the size of the covariance matrix - 2 (n=k-2))
+        n<-ncol(matrix)
+        #volume
+        vol<-pi^(n/2)/gamma((n/2)+1)*prod(eigen.val^(0.5))
+        return(vol)
+
+        #For volume through time use the eigenvectors??
+    }
+
+    # Dummy data
+    dummy_cla <- replicate(50, sample(c(0,1,2), 20, replace=T, prob=c(0.45,0.45,0.1)))
+    rownames(dummy_cla) <- LETTERS[1:20]
+    dummy_tre <- rtree(20, tip.label=LETTERS[1:20])
+
+    # Dummy ordination
+    dummy_dis <- as.matrix(dist(dummy_cla, method="euclidean"))
+    dummy_ord <- cmdscale(dummy_dis, k=19, eig=TRUE)
+    dummy_eig <- dummy_ord$eig
+    dummy_ord <- dummy_ord$points
+    # Calculate the true volume (with eigen values)
+    true_vol <- volume.true(dummy_ord, dummy_eig)
+    # Calculate the volume without the eigen values
+    test_vol <- hyper.volume(dummy_ord)
+    # test
+    expect_equal(true_vol, test_vol)
+
+    # Now testing for PCOA
+    dummy_ord <- pcoa(dummy_dis)
+    dummy_eig <- dummy_ord$values[,1]
+    dummy_ord <- dummy_ord$vectors
+    # Calculate the true volume (with eigen values)
+    true_vol <- volume.true(dummy_ord, dummy_eig)
+    # Calculate the volume without the eigen values
+    test_vol <- hyper.volume(dummy_ord)
+    # test
+    expect_equal(true_vol, test_vol)
+
+
+    # # Testing with eigen
+    # dummy_ord <- eigen(dummy_dis, symmetric=TRUE)
+    # dummy_eig <- dummy_ord$values
+    # dummy_ord <- dummy_ord$vectors
+    # # Calculate the true volume (with eigen values)
+    # true_vol <- volume.true(dummy_ord, dummy_eig)
+    # # Calculate the volume without the eigen values
+    # test_vol <- hyper.volume(dummy_ord)
+    # # test
+    # expect_equal(true_vol, test_vol)
+
+
+
+    # # Now testing with PCA (from cladistic data)
+    # dummy_ord <- prcomp(dummy_cla)
+    # dummy_eig <- dummy_ord$sdev^2 # Squared since the sdev is sqrt(eig)
+    # dummy_ord <- dummy_ord$x
+    # # Calculate the true volume (with eigen values)
+    # true_vol <- volume.true(dummy_ord, dummy_eig)
+    # # Calculate the volume without the eigen values
+    # test_vol <- hyper.volume(dummy_ord)
+    # # test
+    # expect_equal(true_vol, test_vol)
+
+    # ## DOES NOT WORK FOR PCA!
+
+
+    # # Now testing with PCA (from distance data)
+    # dummy_ord <- prcomp(dummy_dis)
+    # dummy_eig <- dummy_ord$sdev^2 # Squared since the sdev is sqrt(eig)
+    # dummy_ord <- dummy_ord$x
+    # # Calculate the true volume (with eigen values)
+    # true_vol <- volume.true(dummy_ord, dummy_eig)
+    # # Calculate the volume without the eigen values
+    # test_vol <- hyper.volume(dummy_ord)
+    # # test
+    # expect_equal(true_vol, test_vol)
+
+    # ## DOES NOT WORK FOR PCA!
+
+    # dummy_ord <- prcomp(dummy_dis, center=FALSE, scale=TRUE)
+    # dummy_eig <- dummy_ord$sdev^2 # Squared since the sdev is sqrt(eig)
+    # dummy_ord <- dummy_ord$x
+    # # Calculate the true volume (with eigen values)
+    # true_vol <- volume.true(dummy_ord, dummy_eig)
+    # # Calculate the volume without the eigen values
+    # test_vol <- hyper.volume(dummy_ord)
+    # # test
+    # expect_equal(true_vol, test_vol) ## Getting closer...
+
+    # #And with svd?
+
+    # svd(dummy_dis)
+})
