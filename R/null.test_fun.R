b73d3830baaba5be48e9bfcdfa49ed6acc5b44e0
Thomas Guillerme <guillert@tcd.ie>
2016-02-28 15:03:53 +0000

    add eval option to get.from.call

--- R/null.test_fun.R
+++ R/null.test_fun.R
@@ -1,12 +1,16 @@
 #Get specific elements from call
-get.from.call <- function(data, what) {
+get.from.call <- function(data, what, eval = TRUE) {
     if(what == "metric") {
         call_out <- strsplit(strsplit(data$call, split = "Disparity calculated as: ")[[1]][[2]], split = " for ")[[1]][[1]]
     }
     if(what == "dimensions") {
         call_out <- strsplit(strsplit(data$call, split = " for ")[[1]][[2]], split = " dimensions")[[1]][[1]]
     }
-    return(eval(parse(text = call_out)))
+    if(eval == TRUE) {
+        return(eval(parse(text = call_out)))
+    } else {
+        return(call_out)
+    }
 }
 
 #Generating the null model
