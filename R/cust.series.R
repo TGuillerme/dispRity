e1db1fff32e8414ec9e8bcc1b983e7cce8de075b
Thomas Guillerme <guillert@tcd.ie>
2016-03-01 12:12:16 +0000

    Isolated internal cust.series functions

--- R/cust.series.R
+++ R/cust.series.R
@@ -60,18 +60,6 @@ cust.series<-function(data, factor) {
     # SPLITING THE DATA INTO A LIST
     #----------------------
 
-    split.elements <- function(X, Y, data) {
-        series_list<-list()
-        for(series in 1:length(levels(as.factor(X)))) {
-            selected_elements<-rownames(Y)[which(as.character(X) == as.character(levels(as.factor(X))[[series]]))]
-            series_list[[series]]<-data[selected_elements,]
-        }
-        ## Adding names to the list (the levels of the custom series)
-        names(series_list)<-levels(as.factor(X))
-        ## Output
-        return(series_list)
-    }
-
     tmp_series<-apply(factor, 2, split.elements, Y=factor, data=data)
     series_list<-unlist(tmp_series, recursive=FALSE)
 
