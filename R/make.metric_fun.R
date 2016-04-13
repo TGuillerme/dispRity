e5f4e505c9dae3ba538ee73f195c74fb4b5ab12f
TGuillerme <guillert@tcd.ie>
2016-04-13 22:27:04 +0100

    make.metric updated for distributions

--- R/make.metric_fun.R
+++ R/make.metric_fun.R
@@ -4,11 +4,17 @@ check.metric<-function(metric) {
     try(some_name.that.probAbly.doesnt.ExIsT<-metric(matrix(rnorm(25),5,5)), silent=TRUE)
     #If function worked
     if(exists("some_name.that.probAbly.doesnt.ExIsT")) {
-        #Is it a summary metric (one value)?
+        #Is it a level 1 metric (one value)?
         if(length(some_name.that.probAbly.doesnt.ExIsT) == 1) {
-            return("summary.metric")
-        } else {
-            return("class.metric")
+            return("level1.metric")
+        }
+        #Is it a level 2 metric (a distribution of values)?
+        if(is.vector(some_name.that.probAbly.doesnt.ExIsT) == TRUE) {
+            return("level2.metric")
+        }
+        #Is it a level 3 metric (a distribution of values)?
+        if(is.matrix(some_name.that.probAbly.doesnt.ExIsT) == TRUE) {
+            return("level3.metric")
         }
     } else {
         stop("Invalid metric.")
