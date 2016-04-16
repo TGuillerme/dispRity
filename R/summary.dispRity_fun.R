082587070515eb1fcdfb909f492a25adf5352bf3
Thomas Guillerme <guillert@tcd.ie>
2016-04-16 17:16:48 +0100

    Changed the recursive enlisting to deal with distributions

--- R/summary.dispRity_fun.R
+++ R/summary.dispRity_fun.R
@@ -1,16 +1,20 @@
 #Bootstrap unlisting
-recursive.unlist<-function(results) {
-    n_series<-length(results)
-    n_rare<-unlist(lapply(results, length))
-    n_bootstraps<-length(results[[1]][[1]])
+recursive.unlist<-function(results, is.distribution = FALSE) {
+    n_series <- length(results)
+    n_rare <- unlist(lapply(results, length))
+    n_bootstraps <- length(results[[1]][[1]])
 
-    series_rare_boot<-list()
+    series_rare_boot <- list()
     for(series in 1:n_series) {
-        rare_boot<-list()
+        rare_boot <- list()
         for (rare in 1:n_rare[series]) {
-            rare_boot[[rare]]<-unlist(results[[series]][[rare]])
+            if(is.distribution == FALSE) {
+                rare_boot[[rare]] <- unlist(results[[series]][[rare]])
+            } else {
+                rare_boot[[rare]] <- results[[series]][[rare]]
+            }
         }
-        series_rare_boot[[series]]<-rare_boot
+        series_rare_boot[[series]] <- rare_boot
     }
     return(series_rare_boot)
 }
