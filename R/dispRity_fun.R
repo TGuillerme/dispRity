2d606173d338bd25351de6541b68fc82982fae52
Thomas Guillerme <guillert@tcd.ie>
2016-06-10 12:19:32 +0100

    Revert "ifelse speed improvements ideas"
    This reverts commit a7aa742bad5c09f0cc4137174a95089914d2cc0f.

--- R/dispRity_fun.R
+++ R/dispRity_fun.R
@@ -5,6 +5,14 @@ lapply_fun<-function(data, fun, ...) {
     return(lapply(data, fun, ...))
 }
 
+# #Calculates disparity metrics
+# disparity.calc<-function(BSresult, class.metric, summary.metric) {
+#     matrix_descriptor<-lapply(BSresult, lapply_fun, fun=class.metric)
+#     matrix_summary<-lapply(matrix_descriptor, lapply_fun, fun=summary.metric)
+#     return(matrix_summary)
+# }
+
+
 disparity.calc<-function(BSresult, level3.fun, level2.fun, level1.fun, ...) {
     #Run level 3 fun (matrix transformation - mat.trans)
     if(!is.null(level3.fun)) {
@@ -13,8 +21,6 @@ disparity.calc<-function(BSresult, level3.fun, level2.fun, level1.fun, ...) {
         matrix_decomposition <- BSresult
     }
 
-    # matrix_decomposition <- ifelse(!is.null(level3.fun), lapply(BSresult, lapply_fun, fun = level3.fun, verbose, ...), BSresult)
-
     #Run level 2 fun (vector aggregate - vec.aggr)
     if(!is.null(level2.fun)) {
         matrix_decomposition <- lapply(matrix_decomposition, lapply_fun, fun = level2.fun, ...)
@@ -22,16 +28,12 @@ disparity.calc<-function(BSresult, level3.fun, level2.fun, level1.fun, ...) {
         matrix_decomposition <- matrix_decomposition
     }
 
-    # matrix_decomposition <- ifelse(!is.null(level2.fun), lapply(BSresult, lapply_fun, fun = level2.fun, verbose, ...), matrix_decomposition)
-
     #Run level 1 fun (vector aggregate - vec.aggr)
     if(!is.null(level1.fun)) {
         matrix_decomposition <- lapply(matrix_decomposition, lapply_fun, fun = level1.fun, ...)
     } else {
         matrix_decomposition <- matrix_decomposition
     }
-
-    # matrix_decomposition <- ifelse(!is.null(level2.fun), lapply(BSresult, lapply_fun, fun = level2.fun, verbose, ...), matrix_decomposition)
 
     return(matrix_decomposition)
 }
