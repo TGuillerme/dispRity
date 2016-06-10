3c4db6085fb80b63dedaedd7b135f5b73661bc15
Thomas Guillerme <guillert@tcd.ie>
2016-06-10 15:13:51 +0100

    Minor speed improvements

--- R/dispRity_fun.R
+++ R/dispRity_fun.R
@@ -175,42 +175,20 @@ get.dispRity.metric.handle<-function(metric) {
     return(list("level3.fun" = level3.fun, "level2.fun" = level2.fun, "level1.fun" = level1.fun))
 }
 
-
-
-#lapply fun
-lapply_fun<-function(data, fun, ...) {
-    return(lapply(data, fun, ...))
-}
-
-# #Calculates disparity metrics
-# disparity.calc<-function(BSresult, class.metric, summary.metric) {
-#     matrix_descriptor<-lapply(BSresult, lapply_fun, fun=class.metric)
-#     matrix_summary<-lapply(matrix_descriptor, lapply_fun, fun=summary.metric)
-#     return(matrix_summary)
-# }
-
-
 disparity.calc<-function(BSresult, level3.fun, level2.fun, level1.fun, ...) {
-    #Run level 3 fun (matrix transformation - mat.trans)
-    if(!is.null(level3.fun)) {
-        matrix_decomposition <- lapply(BSresult, lapply_fun, fun = level3.fun, ...)
-    } else {
-        matrix_decomposition <- BSresult
+
+    lapply_fun<-function(data, fun, ...) {
+        return(lapply(data, fun, ...))
     }
 
-    #Run level 2 fun (vector aggregate - vec.aggr)
-    if(!is.null(level2.fun)) {
-        matrix_decomposition <- lapply(matrix_decomposition, lapply_fun, fun = level2.fun, ...)
-    } else {
-        matrix_decomposition <- matrix_decomposition
-    }
+    matrix_decomposition <- ifelse(!is.null(level3.fun), list(lapply(BSresult, lapply_fun, level3.fun, ...)), list(BSresult))
+    #matrix_decomposition <- ifelse(!is.null(level3.fun), list(lapply(BSresult, lapply_fun, level3.fun)), list(BSresult)) ; warning("DEBUG")
 
-    #Run level 1 fun (vector aggregate - vec.aggr)
-    if(!is.null(level1.fun)) {
-        matrix_decomposition <- lapply(matrix_decomposition, lapply_fun, fun = level1.fun, ...)
-    } else {
-        matrix_decomposition <- matrix_decomposition
-    }
+    matrix_decomposition <- ifelse(!is.null(level2.fun), list(lapply(matrix_decomposition[[1]], lapply_fun, level2.fun, ...)), matrix_decomposition)
+    #matrix_decomposition <- ifelse(!is.null(level2.fun), list(lapply(matrix_decomposition[[1]], lapply_fun, level2.fun)), matrix_decomposition) ; warning("DEBUG")
 
-    return(matrix_decomposition)
+    matrix_decomposition <- ifelse(!is.null(level1.fun), list(lapply(matrix_decomposition[[1]], lapply_fun, level1.fun, ...)), matrix_decomposition)
+    #matrix_decomposition <- ifelse(!is.null(level1.fun), list(lapply(matrix_decomposition[[1]], lapply_fun, level1.fun)), matrix_decomposition) ; warning("DEBUG")
+
+    return(matrix_decomposition[[1]])
 }
