565a83e6f7da9bdfcf6827a40a44570d5e4724ed
Thomas Guillerme <guillert@tcd.ie>
2016-11-30 11:32:36 +0000

    tested time.series

--- R/time.series_fun.R
+++ R/time.series_fun.R
@@ -2,12 +2,8 @@
 ## FAD argument is whether to adjust FAD (TRUE) or LAD (FALSE)
 adjust.FADLAD <- function(FADLAD, tree, data) {
 
-    adjust.age.up <- function(FADLAD, ages_tree) {
-        return(ifelse(FADLAD > ages_tree, FADLAD, ages_tree))
-    }
-
-    adjust.age.down <- function(FADLAD, ages_tree) {
-        return(ifelse(FADLAD < ages_tree, FADLAD, ages_tree))
+    adjust.age <- function(FADLAD, ages_tree) {
+        return(ifelse(FADLAD != ages_tree, FADLAD, ages_tree))
     }
 
     ## Get the tree ages
@@ -19,8 +15,8 @@ adjust.FADLAD <- function(FADLAD, tree, data) {
 
     ## Adjust the FAD/LAD
     ages_tree_FAD <- ages_tree_LAD <- ages_tree_tmp
-    ages_tree_FAD[,1] <- mapply(adjust.age.up,   as.list(FADLAD[,1]), as.list(ages_tree_tmp[,1]))
-    ages_tree_LAD[,1] <- mapply(adjust.age.down, as.list(FADLAD[,2]), as.list(ages_tree_tmp[,1]))
+    ages_tree_FAD[,1] <- mapply(adjust.age, as.list(FADLAD[,1]), as.list(ages_tree_tmp[,1]))
+    ages_tree_LAD[,1] <- mapply(adjust.age, as.list(FADLAD[,2]), as.list(ages_tree_tmp[,1]))
 
     ## Combine all ages
     ages_tree_FAD <- rbind(ages_tree_FAD, ages_tree[-names_match,])
@@ -29,7 +25,7 @@ adjust.FADLAD <- function(FADLAD, tree, data) {
     ## Match the ages with the data
     row_order <- match(rownames(data), ages_tree_FAD$elements)
 
-    return(list("FAD" = ages_tree_FAD[row_order,], "LAD" = ages_tree_FAD[row_order,]))
+    return(list("FAD" = ages_tree_FAD[row_order,], "LAD" = ages_tree_LAD[row_order,]))
 }
 
 
@@ -38,20 +34,13 @@ time.series.discrete <- function(data, tree, time, FADLAD, inc.nodes) {
     ## lapply fun for getting the interval
     get.interval <- function(interval, time, ages_tree, inc.nodes) {
         if(inc.nodes != FALSE) {
-            return( list("elements" = which(ages_tree$FAD$ages <= time[interval] & ages_tree$LAD$ages >= time[interval+1]) ))
+            return( list("elements" = which(ages_tree$FAD$ages > time[interval+1] & ages_tree$LAD$ages < time[interval]) ))
         } else {
-            one_interval <- which(ages_tree$FAD$ages <= time[interval] & ages_tree$LAD$ages >= time[interval+1])
+            one_interval <- which(ages_tree$FAD$ages > time[interval+1] & ages_tree$LAD$ages < time[interval])
             matching <- match(tree$tip.label, rownames(data[one_interval,]))
             return( list("elements" = one_interval[matching[-which(is.na(matching))]]) )
         }
         
-    }
-
-    ## lapply fun for detecting empty intervals
-    empty.interval <- function(one_interval_elements, name) {
-        if(length(one_interval_elements) == 0) {
-            message("The following interval is empty: ", name, ".")
-        }
     }
 
     ## ages of tips/nodes + FAD/LAD
@@ -76,7 +65,7 @@ time.series.discrete <- function(data, tree, time, FADLAD, inc.nodes) {
 ## Continuous time series
 time.series.continuous <- function(data, tree, time, model, FADLAD, verbose) {
     ## lapply fun gor getting the slices
-    get.slice <- function(slice, time, ages_tree, data, verbose) {
+    get.slice <- function(slice, time, model, ages_tree, data, verbose) {
         ## Get the subtree
         if(time[slice] == 0) {
             ## Select the tips to drop
@@ -88,13 +77,19 @@ time.series.continuous <- function(data, tree, time, model, FADLAD, verbose) {
             sub_tree <- slice.tree(tree, time[slice], model, FAD = ages_tree$FAD, LAD = ages_tree$LAD)
         }
 
+        ## Select the tips 
+        tips <- sub_tree$tip.label
+
+        ## Add any missed taxa from the FADLAD
+        taxa <- rownames(data)[which(ages_tree$FAD$ages > time[slice] & ages_tree$LAD$ages < time[slice])]
+
         ## Verbose
         if(verbose) {
             message(".", appendLF = FALSE)
         }
 
         ## Getting the list of elements
-        return( list("elements" = match(sub_tree$tip.label, rownames(data))) )
+        return( list("elements" = match(unique(c(tips, taxa)), rownames(data))) )
     }
 
     ## ages of tips/nodes + FAD/LAD
@@ -106,7 +101,7 @@ time.series.continuous <- function(data, tree, time, model, FADLAD, verbose) {
     }
 
     ## Get the slices elements
-    slices_elements <- lapply(as.list(seq(1:length(time))), get.slice, time, ages_tree, data, verbose)
+    slices_elements <- lapply(as.list(seq(1:length(time))), get.slice, time, model, ages_tree, data, verbose)
 
     ## verbose
     if(verbose != FALSE) {
