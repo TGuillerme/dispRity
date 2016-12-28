87bdb35e589ce7ebf1d483df8fd1ee8a7722c4e9
Thomas Guillerme <guillert@tcd.ie>
2016-12-28 11:07:48 +0000

    transformed the onLoad into proper version numbers

--- R/zzz.R
+++ R/zzz.R
@@ -6,7 +6,7 @@
 
     ## Check version
     if(connection) {
-        last_release <- strsplit(strsplit(RCurl::getURL("https://raw.githubusercontent.com/TGuillerme/dispRity/release/DESCRIPTION", followlocation = TRUE), split = "\nVersion: ")[[1]][2], split = "\nDate:")[[1]][1]
+        last_release <- as.numeric_version(strsplit(strsplit(RCurl::getURL("https://raw.githubusercontent.com/TGuillerme/dispRity/release/DESCRIPTION", followlocation = TRUE), split = "\nVersion: ")[[1]][2], split = "\nDate:")[[1]][1])
         current_version <- packageVersion("dispRity")
         if(current_version < last_release) {
             packageStartupMessage(paste("A newer released version of dispRity (", last_release, ") is available on GitHub.\nTo get the most up to date version, you can use:\n    devtools::install_github(\"TGuillerme/dispRity\", branch = \"release\")", sep = ""))
