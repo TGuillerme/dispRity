7b567f6f9954d5904dd18a862773a8b8a6a1a3f0
Thomas Guillerme <guillert@tcd.ie>
2017-04-14 13:25:01 +0100

    Added char diff plot functions prototypes

--- R/char.diff.R
+++ R/char.diff.R
@@ -1,11 +1,14 @@
 #' @title Character differences
 #'
-#' @description Calculates the character difference from a discrete morphological matrix
+#' @description Calculates the character difference from a discrete matrix
 #'
-#' @param matrix A discrete morphological matrix or a list containing discrete characters.
+#' @param matrix A discrete matrix or a list containing discrete characters.
 #' 
 #' @details
 #' The character difference is calculated as @@@
+#' 
+#' @return
+#' A character difference value or a matrix of class \code{char.diff}
 #' 
 #' @examples
 #' ## Comparing two characters
@@ -47,16 +50,82 @@ char.diff <- function (matrix)  {
         ## Setting the attributes
         attrs <- list(Size = N, Labels = dimnames(matrix)[[1L]], Diag = diag, Upper = upper, method = method, call = match.call(),  class = "dist")
 
-        ## Running the C distance code
-        options(warn = -1)
+        ## Calculating the gower distance
+        options(warn = -1) #TG: NA's get introduced. Don't care!
         output <- as.matrix(.Call("C_char_diff", matrix, method, attrs))
         options(warn = 0)
 
         ## Calculating the character difference
-        return(round( 1 - ( abs(output-0.5)/0.5 ), digit = 10))
+        output <- round( 1 - ( abs(output-0.5)/0.5 ), digit = 10)
+
+        class(output) <- c("matrix", "char.diff")
+
+        return(output)
+
     } else {
+
         check.length(matrix, 2, " must contain only two elements", errorif = FALSE)
         ## Run the slower R version
         return(char.diff_R(matrix[[1]], matrix[[2]]))
+
     }
 }
+
+
+# #' @title Plots character differences
+# #'
+# #' @description Plots a correlation matrix from discrete character differences o
+# #'
+# #' @param matrix A discrete matrix or an already computed character difference matrix of class \code{char.diff}.
+# #' @param type Either \code{"correlation"} (or \code{"c"}) or \code{"density"} (or \code{"d"}) for respectively plotting the correlation or the density of the matrix's characters.
+# #' @param col Two colors for forming the gradient if \code{type = "correlation"} or for the density lines colors if \code{type = "density"}.
+# #' @param legend A logical value stating whether to print the legend or not (default = \code{TRUE}).
+# #' @param legend.title A \code{character} string to be displayed as the title of the legend (default = \code{Difference}).
+# #' @param legend.pos The position of the legend. Can be two \code{numeric}. Default is \code{"topleft"}.
+# #' @param axis A logical value stating whether to print the axis or not (default = \code{TRUE}).
+# #' @param xlim Two \code{numeric} values to determine the x axis limits. If missing (default), the limits are calculated automatically to fit the plot window.
+# #' @param ylim Two \code{numeric} values to determine the y axis limits. If missing (default), the limits are calculated automatically to fit the plot window.
+# #' @param xlab A \code{character} string for the the x axis. Can be missing.
+# #' @param ylab A \code{character} string for the the y axis. Can be missing.
+# #' @param ... Any additional graphical arguments to be passed to \code{image}.
+# #' 
+# #' @examples
+# #' ##
+# #' 
+# #' @author Thomas Guillerme
+# #' @export
+# #' 
+
+# plot.char.diff <- function(matrix, type = "correlation", col = c("blue", "orange"), legend = TRUE, legend.title = "Difference", legend.pos = "topleft", axis = TRUE, xlim, ylim, xlab, ylab, ...) {
+
+#     ## Saving the call
+#     match_call <- match.call()
+
+#     if(!any(class(matrix) == "matrix")) {
+#         stop("")
+#     }
+
+
+#     if(type == "correlation") {
+#         ## Remove the upper triangle
+#         matrix[upper.tri(matrix)] <- NA
+
+#         ## Setting the colours
+#         colfunc <- grDevices::colorRampPalette(col)
+#         colheat <- rev(colfunc(10))
+
+#         ## Plotting the heat map
+#         image(matrix, col = colheat, axes = FALSE, ...)
+#         if(axis) {
+#             axis(1, at = seq(from = 0, to = 1, length.out = ncol(matrix)), labels = FALSE, tick = TRUE)
+#             axis(2, at = seq(from = 0, to = 1, length.out = ncol(matrix)), labels = FALSE, tick = TRUE)
+#         }
+
+#         ## Adding the legend
+#         if(legend) {
+#             legend("topleft", legend = c(as.character(round(max(matrix, na.rm = TRUE), 2)), as.character(round(min(matrix, na.rm = TRUE), 2))), title = legend.title, col = col, pch = 19)
+#         }
+#     } else {
+#         plot.char.diff.density(matrix, main, legend, col, xlim, ylim, legend.pos, xlab, ylab)
+#     }
+# }
--- R/char.diff_fun.R
+++ R/char.diff_fun.R
@@ -81,3 +81,77 @@ char.diff_R <- function(X,Y){
         return( round( 1 - ( abs(sum(abs(differences))/length(X)-0.5)/0.5 ), digit = 10))
     }
 }
+
+
+
+plot.char.diff.density <- function(matrix, main, legend, col, xlim, ylim, legend.pos, xlab, ylab) {
+
+    ## Functions for getting the the density plot limits
+    get.max.x <- function(density) return(max(density$x))
+    get.max.y <- function(density) return(max(density$y))
+    get.min.x <- function(density) return(min(density$x))
+    get.min.y <- function(density) return(min(density$y))
+
+    ## Default options:
+    if(missing(main)) {
+        main = "Character differences profile"
+    }
+    if(missing(legend)) {
+        legend = c("Combined", "Individual")
+    }
+    if(missing(col)) {
+        col = c("black", "grey")
+    }
+    if(missing(xlab)) {
+        xlab = "Character differences"
+    }
+    if(missing(ylab)) {
+        ylab = "Density"
+    }
+    if(missing(legend.pos)) {
+        legend.pos = "topleft"
+    }
+    ## Calculating the character difference matrix
+    chara_diff <- char.diff(matrix)
+
+    ## Removing columns with only NAs
+    select.nas <- function(column) {
+        if((length(column) - length(which(is.na(column)))) <= 2) {
+            return(TRUE)
+        } else {
+            return(FALSE)
+        }
+    }
+    NA_columns <- which(apply(chara_diff, 2, select.nas) == TRUE)
+    if(length(NA_columns) != 0) {
+        chara_diff <- chara_diff[,-NA_columns]
+    }
+
+
+    ## Measuring the densities
+    densities <- apply(chara_diff, 2, density, na.rm = TRUE)
+
+    if(missing(xlim)) {
+        xlim = c(min(unlist(lapply(densities, get.min.x))), max(unlist(lapply(densities, get.max.x))))
+    }
+    if(missing(ylim)) {
+        ylim = c(min(unlist(lapply(densities, get.min.y))), max(unlist(lapply(densities, get.max.y))))
+    }
+
+    ## Measuring the cumulated density
+    cum_density <- density(as.numeric(chara_diff), na.rm = TRUE)
+
+    ## Empty plot
+    plot(1,1, col = "white", xlim = xlim, ylim = ylim, main = main, xlab = xlab, ylab = ylab, bty = "n")
+
+    ## Adding the densities
+    silent <- lapply(densities, lines, col = col[2])
+
+    ## Adding the cumulative density
+    lines(cum_density, col = col[1])
+
+    ## Adding the legend
+    if(legend[1] != FALSE) {
+        legend(legend.pos, legend = legend, lty = 1, col = col)
+    }
+}
