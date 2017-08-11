d234c9c5185f528a2536c672e5e064941a8e9be9
Thomas Guillerme <guillert@tcd.ie>
2017-08-11 16:09:14 +0100

    changed comments

--- R/morpho.utilities.R
+++ R/morpho.utilities.R
@@ -51,24 +51,24 @@
 
 get.contrast.matrix <- function(matrix) {
     
-    # Extracting the states
+    ## Extracting the states
     states <- sort(unique(as.vector(matrix)))
     
-    # Check if there is a "?" token
+    ## Check if there is a "?" token
     if(any(states == "?")) {
-        # remove the "?" state
+        ## remove the "?" state
         states_num <- states[-which(states == "?")]
-        # Create a simple square matrix with 0s...
+        ## Create a simple square matrix with 0s...
         contrast_matrix <- matrix(data = rep(0, length(states_num)*length(states_num)), ncol = length(states_num), dimnames = list(as.character(states_num), as.character(states_num)))
-        # Set the diagonal to 0 
+        ## Set the diagonal to 1
         diag(contrast_matrix) <- 1
-        # Add the joker character as a row full of 1s
+        ## Add the joker character as a row full of 1s
         joker_matrix <- matrix(data = rep(1, length(states_num)), ncol = length(states_num), dimnames = list("?", as.character(states_num)))
         contrast_matrix <- rbind(contrast_matrix, joker_matrix)
     } else {
-        # Create a simple square matrix with 0s...
+        ## Create a simple square matrix with 0s...
         contrast_matrix <- matrix(data = rep(0, length(states)*length(states)), ncol = length(states), dimnames = list(as.character(states), as.character(states)))
-        # Set the diagonal to 0 
+        ## Set the diagonal to 0 
         diag(contrast_matrix) <- 1
     }
 
