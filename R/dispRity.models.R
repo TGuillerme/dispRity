bba9bc6b65f207e7b1d2933356ec90739ed93e8f
Thomas Guillerme <guillert@tcd.ie>
2017-09-14 12:21:54 +0100

    Implemented first 4 simple models

--- R/dispRity.models.R
+++ R/dispRity.models.R
@@ -9,7 +9,7 @@
 #'
 #' @description Different models for changes in disparity through time (\code{BM}, \code{OU}, \code{Stasis} and \code{Trend})
 #'
-#' @param data.model.test A \code{list} of at least four elements: \code{$central_tendency}, \code{$variance}, \cod{$sample_size} and \code{$subsamples}.
+#' @param data.model.test A \code{list} of at least four elements: \code{$central_tendency}, \code{$variance}, \code{$sample_size} and \code{$subsamples}.
 #' @param pool.variance A \code{logical}, whether to pool the variance or not.
 #' @param control.list A \code{list} for fine-tuning control inputs to be passed to the optim function.
 #' @param fixed.optima A \code{logical} to use an estimated optimum value in OU-style models.
@@ -19,10 +19,13 @@
 #' @details
 #' \itemize{
 #'  \item{"BM"}{Fits a unbiased random walk model of evolution (Felsenstein 1985; Hunt 2006). The model optimises the ancestral state and the 'step-variance' (sigma-squared)}
+#' 
 #'  \item{"OU"}{The Ornstein-Uhlenbeck model of evolution in which the change in variance is constrained to an optimum value (Hansen 1997). In this model there are three parameters: optima, alpha, and ancestral state. The strength of attraction based on the parameter alpha and the ancestral state is estimated from the data. The optima value is estimated from the data, and this can lead to optima being found outside the known data values. If this is the case the model is similar to a trend model. If the argument, fixed.optima is set to TRUE, the model will not estimate optima but constrain it to the first value in the sequence}
-#'  \item{"Trend"}{Fits a Brownian motion model with a directional component. This model is also known as the General Random Walk (Hunt 2006). This model has three parameters: the ancestral state, the 'step-variance' (sigma-squared), and the trend component
-#"Stasis" in which traits evolve with variance (omega) around a mean (theta). This model is time-independent in that the model is guided only by the variance and attraction to the mean (Hunt 2006)}
+#' 
+#' \item{"Trend"}{Fits a Brownian motion model with a directional component. This model is also known as the General Random Walk (Hunt 2006). This model has three parameters: the ancestral state, the 'step-variance' (sigma-squared), and the trend component}
+#'  
 #'  \item{"Stasis"}{Fits a model in which traits evolve with variance (omega) around a mean (theta). This model is time-independent in that the model is guided only by the variance and attraction to the mean (Hunt 2006)}
+#' }
 #' 
 #' @return
 #' A list of some sort... @@@@ TO DEVELOP
