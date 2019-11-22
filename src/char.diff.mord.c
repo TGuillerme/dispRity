4ff0630b7f81e0f611d9964fc7244c88859f5e58
TGuillerme <guillert@tcd.ie>
2019-11-22 17:03:16 +1000

    added templated for the four new metrics

---
+++ src/char.diff.mord.c
@@ -0,0 +1,168 @@
+/* 
+ *  char.diff function
+ * 
+ *  Parts of this script are modifications of r-source/src/library/stats/src/distance.c
+ *  from https://github.com/wch/r-source by Thomas Guillerme (guillert@tcd.ie)
+ *
+ *  R : A Computer Language for Statistical Data Analysis
+ *  Copyright (C) 1995, 1996  Robert Gentleman and Ross Ihaka
+ *  Copyright (C) 1998-2016   The R Core Team
+ *  Copyright (C) 2002, 2004  The R Foundation
+ *
+ *  GNU General Public License available at https://www.R-project.org/Licenses/
+ */
+
+
+#include "dispRity.h"
+
+// #################
+// Character difference logic
+// #################
+
+// Calculating the MORD character distance
+static double R_MORD(double *x, int nr, int nc, int i1, int i2)
+{
+    double diff, dist, vector1[nc], vector2[nc];
+    int count, i, k;
+
+    //Initialise the values
+    count= 0;
+    dist = 0;
+
+    //Isolating the two comparable characters
+    for(i = 0 ; i < nc ; i++) {
+        if(both_non_NA(x[i1], x[i2])) {
+            
+            // Create the vectors
+            vector1[count] = x[i1];
+            vector2[count] = x[i2];
+
+            //Increment the counter
+            count++;
+        }
+        i1 += nr;
+        i2 += nr;
+    }
+
+    // Normalising the characters
+    Normalise_single_character(vector1, count);
+    Normalise_single_character(vector2, count);
+
+    // Absolute character difference
+    for(k = 0 ; k < count ; k++) {
+         diff = fabs(vector1[k] - vector2[k]);
+        // Increment the differences
+        if(!ISNAN(diff)) {
+            dist += diff;
+        }        
+    }
+
+    if(count == 0) {
+        return NA_REAL;
+    } else {
+        dist = dist/(count - 1);
+        return dist;
+    }
+}
+
+// R_distance function (R::dist())
+void R_distanceMORD(double *x, int *nr, int *nc, double *d, int *diag, int *method)
+{
+    int dc, i, j;
+    size_t  ij;  /* can exceed 2^31 - 1 */
+    double (*distfun)(double*, int, int, int, int) = NULL;
+
+    //Open MPI
+#ifdef _OPENMP
+    int nthreads;
+#endif
+
+    // switch(*method) {
+    // // case HAMMING:
+    // //     distfun = R_MORD;
+    // //     break;
+    // // case MAXIMUM:
+    // //     distfun = R_maximum;
+    // //     break;
+    // // case MANHATTAN:
+    // //     distfun = R_manhattan;
+    // //     break;
+    // // case CANBERRA:
+    // //     distfun = R_canberra;
+    // //     break;
+    // // case BINARY:
+    // //     distfun = R_dist_binary;
+    // //     break;
+    // // case MINKOWSKI:
+    // //     if(!R_FINITE(*p) || *p <= 0)
+    // //         error(_("distance(): invalid p"));
+    // //     break;
+    // default:
+    //     error(_("distance(): invalid distance"));
+    // }
+
+    distfun = R_MORD;
+
+    dc = (*diag) ? 0 : 1; /* diag=1:  we do the diagonal */
+
+#ifdef _OPENMP
+    if (R_num_math_threads > 0)
+    nthreads = R_num_math_threads;
+    else
+    nthreads = 1; /* for now */
+    if (nthreads == 1) {
+    /* do the nthreads == 1 case without any OMP overhead to see
+       if it matters on some platforms */
+    ij = 0;
+    for(j = 0 ; j <= *nr ; j++)
+        for(i = j+dc ; i < *nr ; i++)
+        d[ij++] = distfun(x, *nr, *nc, i, j);
+    }
+    else
+    /* This produces uneven thread workloads since the outer loop
+       is over the subdiagonal portions of columns.  An
+       alternative would be to use a loop on ij and to compute the
+       i and j values from ij. */
+#pragma omp parallel for num_threads(nthreads) default(none)    \
+    private(i, j, ij)                       \
+    firstprivate(nr, dc, d, method, distfun, nc, x)
+    for(j = 0 ; j <= *nr ; j++) {
+        ij = j * (*nr - dc) + j - ((1 + j) * j) / 2;
+        for(i = j+dc ; i < *nr ; i++)
+        d[ij++] = distfun(x, *nr, *nc, i, j);
+    }
+#else
+    ij = 0;
+    for(j = 0 ; j <= *nr ; j++)
+    for(i = j+dc ; i < *nr ; i++)
+        d[ij++] = distfun(x, *nr, *nc, i, j);
+#endif
+}
+
+
+// R/C interface (former Diff)
+SEXP C_diff_mord(SEXP x, SEXP smethod, SEXP attrs)
+{
+    // Define the variable
+    SEXP result;
+    int nr = nrows(x), nc = ncols(x), method = asInteger(smethod);
+    int diag = 0;
+    R_xlen_t N;
+    N = (R_xlen_t)nr * (nr-1)/2; /* avoid int overflow for N ~ 50,000 */
+    PROTECT(result = allocVector(REALSXP, N));
+    if(TYPEOF(x) != REALSXP) x = coerceVector(x, REALSXP);
+    PROTECT(x);
+    
+    // Calculate the distance matrix
+    R_distanceMORD(REAL(x), &nr, &nc, REAL(result), &diag, &method);
+    
+    // Wrap up the results
+    SEXP names = PROTECT(getAttrib(attrs, R_NamesSymbol)); // Row/column names attributes
+    for (int i = 0; i < LENGTH(attrs); i++) {
+        setAttrib(result, install(translateChar(STRING_ELT(names, i))), VECTOR_ELT(attrs, i));
+    }
+
+    UNPROTECT(3);
+
+    return result;
+}
