5136411832240cbe2cd86da1babeb39187681d94
TGuillerme <guillert@tcd.ie>
2020-03-09 11:25:13 +1000

    Fully updated base demo data

--- R/make.demo.data.R
+++ R/make.demo.data.R
@@ -1,65 +1,67 @@
 ## Developer function for making the disparity data
-make.demo.data_BeckLeeXXX <- function() {
+# make.demo.data_BeckLeeXXX <- function() {
 
-    file.remove("../data/BeckLee_tree.rda")
-    file.remove("../data/BeckLee_ages.rda")
-    file.remove("../data/BeckLee_mat50.rda")
-    file.remove("../data/BeckLee_mat99.rda")
+#     file.remove("../data/BeckLee_tree.rda")
+#     file.remove("../data/BeckLee_ages.rda")
+#     file.remove("../data/BeckLee_mat50.rda")
+#     file.remove("../data/BeckLee_mat99.rda")
 
-    library(dispRity)
-    library(paleotree)
-    library(geiger)
-    source("../tests/testthat/make.data/multi.ace.R")
-    source("../tests/testthat/make.data/convert.tokens.R")
-    source("../tests/testthat/make.data/read.nexus.data.R")
-    ## matrix
-    matrix <- do.call(rbind, read.nexus.data("../tests/testthat/make.data/2014-Beck-ProcB-matrix-morpho.nex"))
+#     library(dispRity)
+#     library(paleotree)
+#     library(geiger)
+#     source("../tests/testthat/make.data/multi.ace.R")
+#     source("../tests/testthat/make.data/convert.tokens.R")
+#     source("../tests/testthat/make.data/read.nexus.data.R")
+#     ## matrix
+#     matrix <- do.call(rbind, read.nexus.data("../tests/testthat/make.data/2014-Beck-ProcB-matrix-morpho.nex"))
+#     ## Remove invariant characters
+#     matrix <- matrix[, -c(28, 47, 50, 74, 81, 92, 103, 141, 142, 154, 155, 158, 159, 220, 231, 232, 237, 250, 255, 272, 276, 283, 284, 362, 363, 377, 380, 400, 417, 420, 421)]
 
-    ## tree
-    tree <- read.nexus("2014-Beck-ProcB-TEM.tre")
-    tree$node.labels <- seq(1:Nnode(tree)) + Ntip(tree)
-    tree_tmp <- extract.clade(tree, 133)
-    tree_tmp <- drop.tip(tree_tmp, extract.clade(tree_tmp, 127)$tip.label)
-    tree <- drop.tip(tree_tmp, c("Erinaceus", "Ptilocercus", "Orycteropus", "Microgale"))
-    tree$root.age <- max(tree.age(tree)$age)
+#     ## tree
+#     tree <- read.nexus("../tests/testthat/make.data/2014-Beck-ProcB-TEM.tre")
+#     tree_tmp <- extract.clade(tree, 133)
+#     tree_tmp <- drop.tip(tree_tmp, extract.clade(tree_tmp, 127)$tip.label)
+#     tree <- drop.tip(tree_tmp, c("Erinaceus", "Ptilocercus", "Orycteropus", "Microgale"))
+#     tree$node.labels <- seq(1:Nnode(tree)) + Ntip(tree)
+#     tree$root.age <- max(tree.age(tree)$age)
 
-    ## Clean the data
-    cleaned_data <- clean.data(matrix, tree)
-    matrix <- cleaned_data$data
-    tree <- cleaned_data$tree
+#     ## Clean the data
+#     cleaned_data <- clean.data(matrix, tree)
+#     matrix <- cleaned_data$data
+#     tree <- cleaned_data$tree
 
-    ## Get the FADLADs
-    FADLAD <- read.csv("Beck2014_FADLAD.csv")
-    FADLAD <- FADLAD[-which(is.na(match(rownames(FADLAD), tree$tip.label))),]
+#     ## Get the FADLADs
+#     FADLAD <- read.csv("../tests/testthat/make.data/Beck2014_FADLAD.csv")
+#     FADLAD <- FADLAD[-which(is.na(match(rownames(FADLAD), tree$tip.label))),]
 
 
-    ## Add the ancestral states estimates
-    ancestral_states <- multi.ace(matrix, tree, models = "ER", verbose = TRUE)[[1]]
-    rownames(ancestral_states) <- tree$node.labels
+#     ## Add the ancestral states estimates
+#     ancestral_states <- multi.ace(matrix, tree, models = "ER", verbose = TRUE)[[1]]
+#     rownames(ancestral_states) <- tree$node.labels
 
-    ## Combine both
-    matrix_tips <- matrix
-    matrix_tips_nodes <- rbind(matrix, ancestral_states)
+#     ## Combine both
+#     matrix_tips <- matrix
+#     matrix_tips_nodes <- rbind(matrix, ancestral_states)
 
-    ## Measuring the distance
-    distance_matrix_tips_nodes <- char.diff(matrix_tips_nodes, by.col = FALSE)
-    distance_matrix_tips <- char.diff(matrix_tips, by.col = FALSE)
+#     ## Measuring the distance
+#     distance_matrix_tips_nodes <- char.diff(matrix_tips_nodes, by.col = FALSE)
+#     distance_matrix_tips <- char.diff(matrix_tips, by.col = FALSE)
 
-    ## Ordination (just because)
-    pco_tips <- cmdscale(distance_matrix_tips, k = nrow(distance_matrix_tips) - 2, add = TRUE)$points
-    pco_tips_nodes <- cmdscale(distance_matrix_tips_nodes, k = nrow(distance_matrix_tips_nodes) - 2, add = TRUE)$points
+#     ## Ordination (just because)
+#     pco_tips <- cmdscale(distance_matrix_tips, k = nrow(distance_matrix_tips) - 2, add = TRUE)$points
+#     pco_tips_nodes <- cmdscale(distance_matrix_tips_nodes, k = nrow(distance_matrix_tips_nodes) - 2, add = TRUE)$points
 
-    BeckLee_tree <- tree
-    BeckLee_ages <- FADLAD
-    BeckLee_mat50 <- pco_tips
-    BeckLee_mat99 <- pco_tips_nodes
+#     BeckLee_tree <- tree
+#     BeckLee_ages <- FADLAD
+#     BeckLee_mat50 <- pco_tips
+#     BeckLee_mat99 <- pco_tips_nodes
 
-    ## save the data
-    save(BeckLee_ages, file = "../data/BeckLee_ages.rda")
-    save(BeckLee_tree, file = "../data/BeckLee_tree.rda")
-    save(BeckLee_mat50, file = "../data/BeckLee_mat50.rda")
-    save(BeckLee_mat99, file = "../data/BeckLee_mat99.rda")
-}
+#     ## save the data
+#     save(BeckLee_ages, file = "../data/BeckLee_ages.rda")
+#     save(BeckLee_tree, file = "../data/BeckLee_tree.rda")
+#     save(BeckLee_mat50, file = "../data/BeckLee_mat50.rda")
+#     save(BeckLee_mat99, file = "../data/BeckLee_mat99.rda")
+# }
 
 
 
