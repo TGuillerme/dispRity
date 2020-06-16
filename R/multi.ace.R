86078cefb7f6210c452aab7d6951a9a242f598a4
TGuillerme <guillert@tcd.ie>
2020-06-16 15:54:18 +0100

    parallel option

--- R/multi.ace.R
+++ R/multi.ace.R
@@ -123,56 +123,6 @@
 #' @author Thomas Guillerme
 #' @export
 
-# library(ape)
-# library(Claddis)
-# library(dispRity)
-# source("get.taxa.ages.R")
-# source("read.nexus.data.R") ## While waiting for ape 5.4
-# source("MakeMorphMatrix.R") ## Updated from Claddis
-# source("AncStateEstMatrixFast.R") ## Updated from Claddis
-
-# source("convert.tokens.R")
-
-# morpho_matrix <- read.nexus.data("../Data/Morphology/227t_682c_morphology.nex")
-# matrix <- do.call(rbind, morpho_matrix)
-# no_overlap <- reduce.matrix(ifelse(matrix == "?" | matrix == "-", NA, 1))
-# no_overlap$rows.to.remove
-# matrix <- morpho_matrix[-which(names(morpho_matrix) %in% no_overlap$rows.to.remove)]
-# load("../Data/Processed/tree_list.Rda")
-# tree <- tree_list[[1]]
-# use.poly = FALSE
-# use.uncertain = FALSE
-# use.inapp = FALSE
-# models <- "ER"
-# parallel = FALSE
-# special.tokens <- list()
-# threshold = TRUE
-# verbose = TRUE
-
-
-# tree <- list(tree, tree, tree, tree)
-# class(tree) <- "multiPhylo"
-
-
-# test <- multi.ace(matrix, tree, models = "ER", use.poly = TRUE, use.uncertain = TRUE, verbose = TRUE)
-
-# matrix <- morpho_matrix
-# tree <- cal3_trees
-# use.poly = TRUE
-# use.uncertain = TRUE
-# use.inapp = FALSE
-# models <- "ER"
-# parallel = FALSE
-# special.tokens <- list()
-# threshold = TRUE
-# verbose = TRUE
-
-
-##TODO: allow tree to be a multiPhylo object + a sample element that randomly samples a tree everytime and runs ACE on all trees?
-
-##TODO: add node label names
-##TODO: if input is matrix, output is combined matrix (option add = TRUE default)
-
 multi.ace <- function(data, tree, models = "ER", threshold = TRUE, special.tokens, special.behaviours, brlen.multiplier, verbose = FALSE, parallel = FALSE, output, castor.options) {
 
     match_call <- match.call()
@@ -197,14 +147,6 @@ multi.ace <- function(data, tree, models = "ER", threshold = TRUE, special.token
         class(tree) <- "multiPhylo"
     }
     ## Find the node labels (and eventually add them to the trees)
-    get.node.labels <- function(tree) {
-        if(is.null(tree$node.label)) {
-            tree$node.label <- seq((Ntip(tree)+1):(Ntip(tree)+Nnode(tree)))
-            return(list(tree, tree$node.label))
-        } else {
-            return(list(tree, tree$node.label))
-        }
-    }
     node_labels <- lapply(tree, get.node.labels)
     ## Split the trees and the labels
     tree <- lapply(node_labels, `[[`, 1)
@@ -231,13 +173,6 @@ multi.ace <- function(data, tree, models = "ER", threshold = TRUE, special.token
                     check.length(models, n_characters, msg = paste0(" should be list of characters or/and matrices of length ", ncol(matrix), "."))
 
                     ## Check all models
-                    check.model.class <- function(one_model, available_models) {
-                        if(class(one_model)[[1]] == "character") {
-                            check.method(one_model, available_models, msg = "models")
-                        } else {
-                            check.class(c(one_model), c("numeric", "integer"), msg = "models must be numerical matrices")
-                        }
-                    }
                     silent <- lapply(models, check.model.class, available_models)
                 }
         }
@@ -442,51 +377,11 @@ multi.ace <- function(data, tree, models = "ER", threshold = TRUE, special.token
     }
     if(verbose) cat(".")
 
-    ## Convert the characters into probability tables
-    convert.char.table <- function(character, character_states) {
-        convert.one.taxon <- function(taxon, character_states) {
-            ## Number of states
-            n_states <- length(character_states)
-            if(all(is.na(taxon))) {
-                ## If all NA, return only NAs
-                return(rep(NA, n_states))
-            } else {
-                ## Make the empty table
-                table <- rep(0, n_states)
-                ## Fill the table
-                if(any(active_states <- character_states %in% taxon)) {
-                    ## Fill in the character states
-                    table[active_states] <- 1/length(which(active_states))
-                } else {
-                    ## Return only NAs (should normally never get ticked)
-                    return(rep(NA, n_states))
-                }
-                return(table)
-            }
-        }
-        return(t(sapply(character, convert.one.taxon, character_states)))
-    }
-
     ## Get the character tables
     characters_tables <- mapply(convert.char.table, characters, characters_states, SIMPLIFY = FALSE)
     if(verbose) cat(".")
 
-    ## Set up the characters arguments
-    make.args <- function(character, character_states, model, castor.options, cores) {
-        ## Get the list of arguments
-        castor_args <- list(tip_states = NULL,
-                            Nstates = length(character_states),
-                            rate_model = model,
-                            tip_priors = character,
-                            check_input = FALSE,
-                            Ntrials = 1,
-                            Nthreads = cores)
-        ## Add options
-        if(!is.null(castor.options)) {
-            castor.options <- c(castor_args, castor.options)
-        }
-        return(castor_args)
-    }
+    ## Set up the arguments for one tree
     args_list <- mapply(make.args, characters_tables, characters_states, models,
                         MoreArgs = list(castor.options, cores), SIMPLIFY = FALSE)
 
@@ -498,162 +393,54 @@ multi.ace <- function(data, tree, models = "ER", threshold = TRUE, special.token
 
     if(verbose) cat("Done.\n")
 
-    ## Function for running ace on a single tree.
-    one.tree.ace <- function(args_list, special.tokens, invariants, threshold.type, threshold, verbose) {
 
-        ## Update the tree and data
-        update.tree.data <- function(castor_args) {
-            ## Find if any of the tips have NAs
-            if(any(dropped <- apply(castor_args$tip_priors, 1, FUN = function(x) any(is.na(x))))) {
-                ## Update the tip_priors
-                castor_args$tip_priors <- castor_args$tip_priors[!dropped,]
-                ## Get the subtree
-                subtree <- castor::get_subtree_with_tips(castor_args$tree, omit_tips = names(which(dropped)))
-                ## Get the dropped nodes (missing ones are original order)
-                dropped <- list("missing" = castor_args$tree$node.label[-subtree$new2old_node], "order" = castor_args$tree$node.label)
-                ## Update the tree
-                castor_args$tree <- subtree$subtree
-            } else {
-                dropped <- NULL
-            }
-            return(list(castor_args, dropped = dropped))
+    if(do_parallel) {
+        ## Remove verbose
+        if(verbose) {
+            cat(paste0("Running the estimation for ", length(tree), " tree", ifelse(length(tree) > 1, "s ", " "), "using ", cores, " core", ifelse(cores == 1, "...", "s...")))
+            was_verbose <- TRUE
+            verbose <- FALSE
         }
 
-        ## Estimate the ancestral states
-        castor.ace <- function(castor_args) {
-            verboseplaceholder <- "silent"
+        ## Set up the cluster
+        cluster <- parallel::makeCluster(cores)
 
-            ## Drop the tips with no data if needed
-            dropped <- update.tree.data(castor_args)
-            castor_args <- dropped[[1]]
-            dropped <- dropped[[2]]
+        ## Get the current environment
+        current_env <- environment()
+        ## Get the export lists
+        export_arguments_list <- c("tree_args_list",
+                                   "special.tokens",
+                                   "invariants",
+                                   "threshold.type",
+                                   "threshold",
+                                   "verbose",
+                                   "characters_states")
+        export_functions_list <- c("castor.ace",
+                                   "update.tree.data",
+                                   "add.state.names",
+                                   "translate.likelihood")
 
-            ## Run the ace
-            estimation <- do.call(castor::asr_mk_model, castor_args)
+        ## Export from this environment
+        parallel::clusterExport(cluster, c(export_arguments_list, export_functions_list), envir = current_env)
 
-            ## Increase the number of trials if unsuccessful
-            while(!estimation$success && trials < 100) {
-                castor_args$Ntrials <- castor_args$Ntrials + castor_args$Ntrials * 2
-                estimation <- do.call(castor::asr_mk_model, castor_args)
-            }
+        ## Call the cluster
+        results_out <- parLapply(cl = cluster, tree_args_list, one.tree.ace, special.tokens, invariants, threshold.type, threshold, verbose)
 
-            ## Get the likelihoods
-            if(!estimation$success) {
-                estimation <- matrix(1/castor_args$Nstates, ncol = castor_args$Nstates, nrow = Nnode(tree))
-                success <- FALSE
-            } else {
-                estimation <- estimation$ancestral_likelihoods
-                success <- TRUE
-            }
+        ## Stop the cluster
+        parallel::stopCluster(cluster)
 
-            ## Add back the missing nodes
-            if(!is.null(dropped)) {
-                ## Add the missing nodes
-                missing_estimations <- matrix(NA, ncol = castor_args$Nstates, nrow = length(dropped$missing),
-                                              dimnames = list(dropped$missing))
-                rownames(estimation) <- castor_args$tree$node.label
-                ## Order them following the normal tip order
-                estimation <- rbind(estimation, missing_estimations)[dropped$order,]
-            } else {
-                ## Add the node labels
-                rownames(estimation) <- castor_args$tree$node.label
-            }
-
-            return(list(results = estimation, success = success, dropped = dropped))
+        ## Reactivate the verbose
+        if(was_verbose) {
+            cat("Done.")
+            verbose <- TRUE
         }
-
-        ## Update names and trees a posteriori
-        add.state.names <- function(estimation, character_states, tree) {
-            ## Update the estimation names
-            colnames(estimation$results) <- character_states
-
-            if(!is.null(estimation$dropped)) {
-                ## Update the estimations table
-            }
-
-            return(estimation)
-        }
-
-        if(verbose) body(castor.ace)[[2]] <- substitute(cat("."))
-        if(verbose) cat("Running ancestral states estimations:\n")
-        ancestral_estimations <- lapply(args_list, castor.ace)
-        ancestral_estimations <- mapply(add.state.names, ancestral_estimations, characters_states, SIMPLIFY = FALSE)
-        if(verbose) cat(" Done.\n")
-
-        ## Separating the estimations
-        success <- unlist(lapply(ancestral_estimations, function(estimation) return(estimation$success)))
-        ancestral_estimations <- lapply(ancestral_estimations, function(estimation) return(estimation$results))
-        if(any(!success)) {
-            warning(paste0("Impossible to fit the model for the following character(s): ", paste(which(!success), collapse = ", "), ".\nThe ancestral estimated values are set to uncertain (all states equiprobable)."))
-        }
-
-        ## Select the threshold type function
-        switch(threshold.type,
-            relative = {select.states <- function(taxon, threshold) {
-                                                  if(all(is.na(taxon))) {
-                                                    return(NA)
-                                                  } else {
-                                                    return(names(taxon[taxon >= (max(taxon) - 1/length(taxon))]))
-                                                  }
-                                                 }},
-            max      = {select.states <- function(taxon, threshold) {
-                                                  if(all(is.na(taxon))) {
-                                                    return(NA)
-                                                  } else {
-                                                    return(names(taxon[taxon >= (max(taxon))]))
-                                                  }
-                                                 }},
-            absolute = {select.states <- function(taxon, threshold) {
-                                                  if(all(is.na(taxon))) {
-                                                    return(NA)
-                                                  } else {
-                                                    return(names(taxon[taxon >= threshold]))
-                                                  }
-                                                 }})
-        
-        ## Translating the likelihood table into a vector of characters
-        translate.likelihood <- function(character, threshold, select.states, special.tokens) {
-            ## Translate the likelihood table
-            threshold.fun <- function(taxon, threshold, select.states, special.tokens) {
-                return(paste(select.states(taxon, threshold), collapse = sub("\\\\", "", special.tokens["uncertainty"])))
-            }
-            translated_states <- apply(character, 1, threshold.fun, threshold, select.states, special.tokens)
-            ## Replace empty states by uncertainties
-            replace.empty.states <- function(x, character, special.tokens) {
-                return(ifelse(x == "", paste(colnames(character), collapse = sub("\\\\", "", special.tokens["uncertainty"])), x))
-            }
-            translated_states <- replace.empty.states(translated_states, character, special.tokens) 
-            ## Return translation
-            return(translated_states)
-        }
-
-        ## Estimate the ancestral states
-        ancestral_states <- lapply(ancestral_estimations, translate.likelihood, threshold, select.states, special.tokens)
-
-        ## Add invariants characters back in place
-        if(length(invariants) > 0) {
-            ## Replicate the invariant characters
-            invariant_ancestral <- lapply(invariant_characters_states, function(x, n) rep(ifelse(length(x == 0), special.tokens["missing"], x), n), args_list[[1]]$tree$Nnode)
-
-            ## Combine the final dataset
-            output <- replicate(ncol(matrix), list())
-            ## Fill the final dataset
-            output[invariants] <- invariant_ancestral
-            output[-invariants] <- ancestral_states
-            ancestral_states <- output
-        }
-        return(ancestral_states)
+    } else {
+        ## Running the ACE for each tree
+        results_out <- lapply(tree_args_list, one.tree.ace, special.tokens, invariants, threshold.type, threshold, verbose)
     }
 
-    ## Running the ACE for each tree
-    results_out <- lapply(tree_args_list, one.tree.ace, special.tokens, invariants, threshold.type, threshold, verbose)
-
-    warning("add parallel option")
-
     ## Output a matrix
-    make.matrix <- function(results) {#, node_labels) {
-        # results <- lapply(results, function(data) do.call(cbind, data))
-        # return(mapply(function(result, names) {rownames(result) <- names; return(result)}, results, node_labels, SIMPLIFY = FALSE))
+    make.matrix <- function(results) {
         return(lapply(results, function(data) do.call(cbind, data)))
     }
     ## Combine the ace matrix with the tips
