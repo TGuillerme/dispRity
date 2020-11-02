7f8283a7b8309d0a39368d5e470dba64fc8d074b
TGuillerme <guillert@tcd.ie>
2020-11-02 18:29:01 +0000

    updated tes from castor change

--- tests/testthat/test-multi.ace.R
+++ tests/testthat/test-multi.ace.R
@@ -149,7 +149,9 @@ test_that("multi.ace works", {
     expect_is(results[[1]], "list")
     expect_is(results[[1]][[1]], "character")
     expect_equal(results[[1]][[1]], c("0", "0/1/2", "0/1", "0", "0", "0/1", "1", "0", "0", "0/1"))
-    expect_equal(results[[2]][[4]], c("0", "0", "0", "0", "0/1", "0/1", "1", "0", "0", "1"))
+    # expect_equal(results[[2]][[4]], c("0", "0", "0", "0", "0/1", "0/1", "1", "0", "0", "1"))
+    expect_equal(results[[2]][[4]], c("0", "0", "0", "0", "0", "0/1", "1", "0", "0", "1"))
+    # new version of castor...
 
     ## Some specific case
     list_matrix <- unlist(apply(matrix_test, 1, list), recursive = FALSE)
