1613fd50bd31ceef2e1a892edd05c6992ae2d3ec
TGuillerme <guillert@tcd.ie>
2021-10-13 16:45:58 +0100

    added dispRity fast

---
+++ tests/testthat/test-dispRity.fast.R
@@ -0,0 +1,16 @@
+## Test
+
+test_that("dispRity.fast works", {
+    ## Basic testing
+    space <- matrix(1, 5, 5)
+    metric1 <- sum
+    metric2 <- c(sum, variances)
+    metric3 <- variances
+    metric4 <- centroids
+    group <- c(TRUE, TRUE, TRUE, TRUE, FALSE)
+    ## Testing metrics
+    expect_equal(dispRity.fast(group, space, metric1), 20)
+    expect_equal(dispRity.fast(group, space, metric2), 0)
+    expect_equal(dispRity.fast(group, space, metric3), c(0,0,0,0,0))
+    expect_equal(dispRity.fast(group, space, metric4), c(0,0,0,0))
+})
