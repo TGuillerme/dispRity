cfae52568b91ec24678faf9838eaeaa0a8f9176a
Thomas Guillerme <guillert@tcd.ie>
2023-04-25 11:11:15 +0100

    Update export.cran.sh

--- export.cran.sh
+++ export.cran.sh
@@ -69,7 +69,7 @@ rm src/*.rds
 version_number=$(grep "Version:" DESCRIPTION | sed 's/Version: //g')
 
 ## CHANGE THE WARNING zzz.R
-sed 's/# //g' R/zzz.R | sed 's/GitHub release./This is the CRAN release version ('"$version_number"') of the package.\\nFor news, vignettes and future releases,\\nvisit https:\/\/github.com\/TGuillerme\/dispRity/g' > ${TMPPATH}/R/zzz.R
+sed 's/# //g' R/zzz.R | sed 's/GitHub release./This is the CRAN released version of the package ('"$version_number"').\\nFor news, vignettes and future releases, visit\\nhttps:\/\/github.com\/TGuillerme\/dispRity\\nTo cite the package use\\ncitation("dispRity")/g' > ${TMPPATH}/R/zzz.R
 
 ## Add ssptest.support and remove dependencies 
 cp ~/Packaging/CRAN/Support/ssptest.support.R ${TMPPATH}/R/
