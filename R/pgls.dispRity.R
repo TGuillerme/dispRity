eaec96bebc34b3508edf12d1ef5e23f75c5adbd3
Thomas Guillerme <guillert@tcd.ie>
2023-08-03 17:01:02 +0100

    sanitizing for the three first terms

--- R/pgls.dispRity.R
+++ R/pgls.dispRity.R
@@ -4,7 +4,8 @@
 #'
 #' @param data A \code{dispRity} object with a metric of dimension level 2 at least
 #' @param tree If \code{data} does not contain a \code{tree} component, a \code{"phylo"} object to be used as the tree.
-#' @param formula Optional, the PGLS formula. If left empty, runs either \code{disparity ~ 1} or \code{disparity ~ subsets} if \code{data} contains subsets.
+#' @param formula The PGLS formula. If left empty, runs either \code{disparity ~ 1} or \code{disparity ~ subsets} if \code{data} contains subsets.
+#' @param model The covariance model (default is \code{"BM"}). For more details (including the models available) see the manual for \code{\link[phylolm]{phylolm}}.
 #' @param ... Any optional arguments to be passed to \code{\link[phylolm]{phylolm}}
 #' @param optim An optional named list of arguments to be passed to the function \code{optim}
 #' 
@@ -21,22 +22,72 @@
 #' 
 #' @author Thomas Guillerme
 
-pgls.dispRity <- function(data, tree, formula, ..., optim = list()) {
+pgls.dispRity <- function(data, tree, formula, model = "BM", ..., optim = list()) {
 
-    ## Check data (level 2)
+    match_call <- match.call()
+
+    ## Check data
+    check.class(data, "dispRity")
+    ## Check data level
+    disparity_list <- get.disparity(data)
+    checks <- unlist(lapply(disparity_list, check.dimension))
+    if(any(!checks)) {
+        stop.call(msg.pre =  "Impossible to run a univariate pgls on ", match_call$data, msg = " because doesn't contain a dimension level-2 metric. See ?dispRity.metric for more info.")
+    }
 
     ## Check tree in data
-        ## If tree is given, replace the tree in data
+    if(is.null(unlist(data$tree))) {
+        ## Check the tree format
+        data <- add.tree(data, tree = tree)
+    }
+    ## Get the trees
+    trees <- get.tree(data)
+
 
     ## Check the formula
-        ## If missing
-            ## the first term is disparity ~ 
+    if(missing(formula)) {
+        ## Handle the formula depending on what is in data
+        formula <- disparity ~ 1
+
             ## If data has subsets
                 ## Set model to disparity ~ groups
                 ## Else, set to disparity ~ 1
-        ## Regardless check if response is "disparity"
+
+
+    }
+    ## Check if response is disparity
+    if(as.character(formula[[2]]) != "disparity") {
+        stop("The response term of the formula must be 'disparity'.", call. = FALSE)
+    }
+
+    ## Check model
+
+    # ## Set the phylolm args
+    # phylolm_args <- list(...)
+    # phylolm_args$formula <- 
+    # phylolm_args$model <-
+
+    # ## Handle the multiple matrices
+    # phylolm_args$data <-
+    # ## Handle the multiple trees
+    # phylolm_args$tree <-
+
+    # ## Run all the PGLS
+    # do.call(phylolm, phylolm_args)
 
     ## Run PGLS on all the trees
 
-    ##
+    ## Handle outputs
+
+    return(NULL)
+}
+
+## Internals
+## Check the dimension of the data
+check.dimension <- function(one_disparity) {
+    ## Must have names, be numeric (or integer), no dimensions and length > 1
+    return(!is.null(names(one_disparity)) &&
+           (is.numeric(one_disparity) || is.integer(one_disparity)) &&
+           is.null(dim(one_disparity)) &&
+           length(one_disparity) > 1)
 }
--- tests/testthat/test-pgls.dispRity.R
+++ tests/testthat/test-pgls.dispRity.R
@@ -1,5 +1,55 @@
 test_that("sanitizing works", {
 
+    ## Simple examples
+    data(BeckLee_tree)
+    data(BeckLee_mat99)
+    data(BeckLee_mat50)
+    data(BeckLee_ages)
+    nonode_tree <- BeckLee_tree
+    nonode_tree$node.labels <- NULL
+
+    ## Base examples
+    disparity_base <- dispRity(BeckLee_mat50, metric = centroids)
+    disparity_group <- dispRity(custom.subsets(BeckLee_mat50, group = crown.stem(BeckLee_tree, inc.nodes = FALSE)), tree = nonode_tree, metric = centroids)
+    expect_warning(disparity_time <- dispRity(chrono.subsets(BeckLee_mat50, FADLAD = BeckLee_ages, tree = BeckLee_tree, time = 5, method = "discrete"), metric = centroids, tree = nonode_tree))
+
+    ## Sanitizing
+    ## data
+    wrong_dimensions <- dispRity(BeckLee_mat50, metric = c(sum, variances))
+    error <- capture_error(pgls.dispRity(data = "wrong_dimensions", tree = BeckLee_tree))
+    expect_equal(error[[1]], "data must be of class dispRity.")
+    error <- capture_error(pgls.dispRity(data = wrong_dimensions, tree = BeckLee_tree))
+    expect_equal(error[[1]], "Impossible to run a univariate pgls on wrong_dimensions because doesn't contain a dimension level-2 metric. See ?dispRity.metric for more info.")
+    
+    ## tree
+    wrong_tree <- rtree(50)
+    error <- capture_error(pgls.dispRity(data = wrong_dimensions, tree = "wrong_tree"))
+    expect_equal(error[[1]], "tree must be of class multiPhylo or phylo.")
+    expect_warning(error <- capture_error(pgls.dispRity(data = wrong_dimensions, tree = wrong_tree)))
+    expect_equal(error[[1]], "The data is not matching the tree labels (you can use ?clean.data to match both data and tree).")
+
+    ## formula
+    wrong_formula <- dispoority ~ 1
+    error <- capture_error(pgls.dispRity(data = disparity_base, tree = BeckLee_tree, formula = wrong_formula))
+    expect_equal(error[[1]], "The response term of the formula must be 'disparity'.")
+
+    ## model
+
+
+    ## Testing super simple
+    data <- disparity_base
+
+
+    ## Multiple trees example
+    set.seed(1)
+    trees <- replicate(3, rcoal(10), simplify = FALSE)
+    class(trees) <- "multiPhylo"
+    disparities <- replicate(3, matrix(rnorm(30), ncol = 3, dimnames = list(paste0("t", 1:10))), simplify = FALSE)
+
+    ## Multiple matrices example
+
+    ## Multiple trees and matrices
+
     ## Checks whether the dispRity object contains a tree or whether a tree can be added
 
     ## Check whether the dispRity object is a level 2 metric at least
