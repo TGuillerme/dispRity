dbce0313ffc04a0fbd69d3e6b905be35d43b09ad
Thomas Guillerme <guillert@tcd.ie>
2024-05-22 16:43:46 +0100

    BAT.,metric UP

--- R/BAT.metric.R
+++ R/BAT.metric.R
@@ -4,35 +4,31 @@
 #'
 #' @param matrix A data matrix.
 #' @param BAT.fun The name of the metric or directly it's function.
-#' @param ... Any optional arguments to be passed to \code{BAT.metric}
+#' @param BAT.args Any named optional arguments to be passed to \code{BAT.metric} (default is \code{NULL})
 #'
 #' @details
 #' This function is an interface between the \code{dispRity} and B\code{BAT}AT package allowing to use a \code{BAT} function in the \code{dispRity} pipeline.
 #' By default the function uses the \code{dispRity.metric} format:
 #'
-#' \code{metric_value <- BAT.metric(matrix, BAT.fun = BAT::alpha, ...)}
+#' \code{metric_value <- BAT.metric(matrix, BAT.fun = BAT::alpha)}
 #'
 #' or
 #'
-#' \code{metric_value <- BAT.metric(matrix, BAT.fun = "alpha", ...)} 
+#' \code{metric_value <- BAT.metric(matrix, BAT.fun = "alpha")} 
 #'
-#' With any optional argument being handled normally.
+#' or
+#'
+#' \code{metric_value <- BAT.metric(matrix, BAT.fun = alpha)} 
+#'
+#' With any optional argument being handled normally through \code{BAT.args}.
+#'
 #' However, most commonly, the function can be used in the context of the \code{dispRity} pipeline as follows:
 #'
-#' \code{my_disparity <- dispRity(my_data, metric = BAT.metric, BAT.fun = BAT::alpha, ...)}
+#' \code{my_disparity <- dispRity(my_data, metric = BAT.metric, BAT.fun = BAT::alpha)}
 #'
-#' or
+#' \emph{NOTE} that if the \code{dispRity} object contains a \code{$tree} element. It is recycled to the BAT metric if available as an optional argument.
+#' You can override this behaviour by providing your own tree (e.g. using \code{BAT.args = list(tree = my_tree)}) or by removing the tree from your input data (e.g. using \code{remove.tree(my_data)}).
 #'
-#' \code{metric_value <- dispRity(my_data, metric = BAT.metric, BAT.fun = "alpha", ...)} 
-#'
-#' The default argument \code{tree} is handled differently depending on the context:
-# TODO: contexts for tree
-#' \itemize{
-#'      \item if...
-#'      \item if...
-#'      \item if...
-#' }
-#' 
 #' @examples
 #' ## Base example:
 #' ## Generating a matrix
@@ -50,12 +46,14 @@
 #' ## Apply the alpha diversity on these subsets
 #' alpha_diversity <- dispRity(eco_data, metric = BAT.metric, BAT.fun = "alpha")
 #' summary(alpha_diversity)
+#'
 #' @seealso \code{\link{dispRity}}, \code{\link{custom.subsets}}
 #' 
 #' @author Thomas Guillerme
-BAT.metric <- function(matrix, BAT.fun, ...) {
+BAT.metric <- function(matrix, BAT.fun, BAT.args = NULL) {
     
     #SANITIZNG
+    match_call <- match.call()
 
     ## Checking the matrix
     check.class(matrix, "matrix")
@@ -64,6 +62,9 @@ BAT.metric <- function(matrix, BAT.fun, ...) {
     BAT.fun_class <- check.class(BAT.fun, c("function", "character"))
     if(BAT.fun_class == "function") {
         ## Check if the function arguments look like BATlike
+        if(!("comm" %in% names(formals(BAT.fun)))) {
+            stop.call(msg.pre = "The function ", call = match_call$BAT.fun, msg = " doesn't look like it's formatted in the BAT style (missing the \"comm\" argument).")
+        }
     } else {
         ## Use the implemented functions
         recognised_function_names <- c("alpha")
@@ -73,16 +74,6 @@ BAT.metric <- function(matrix, BAT.fun, ...) {
         BAT.fun <- eval(str2lang(paste0("BAT::", BAT.fun)))
     }
 
-    # ## Handeling tree
-    # dots <- list(...)
-    # tree_arg <- NULL
-    # if(any(names(dots) %in% "tree")) {
-    #     tree <- dots$tree
-    #     ## Check how the tree needs to be handled by BAT
-    #     check.class(tree, c("phylo", "hclust"))
-    #     tree_arg <- tree   
-    # }
-
     ## Check if the function needs a tree
     dendro_tree_arg <- names(formals(BAT.fun))[2]
 
@@ -91,13 +82,15 @@ BAT.metric <- function(matrix, BAT.fun, ...) {
     ## Basic
     comm_arg <- make.BAT.comm(matrix)
 
-    ## Handle BAT args
-    # if(!is.null(tree_arg)) {
-    #     BAT_args <- list(comm = comm_arg, second = tree_arg, ...)
-    #     names(BAT_args)[2] <- dendro_tree_arg
-    # } else {
-        BAT_args <- list(comm = comm_arg, ...)
-    # }
+    ## Handle the arguments
+    if(!is.null(BAT.args)) {
+        BAT_args <- BAT.args
+    } else {
+        BAT_args <- list()
+    }
+
+    ## Add the comm argument
+    BAT_args$comm <- comm_arg
 
     ## Run the fun!
     return(do.call(BAT.fun, BAT_args)[[1]])
