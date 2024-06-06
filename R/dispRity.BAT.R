854f34c77c83a5aed2a7c7e02ca86684b2465c34
Thomas Guillerme <guillert@tcd.ie>
2024-06-06 13:03:58 +0100

    removed BAT bits for merge

--- R/dispRity.BAT.R
+++ R/dispRity.BAT.R
@@ -1,164 +1,164 @@
-#' @title Converts \code{dispRity} to \code{BAT}.
-#'
-#' @description Converts a \code{dispRity} object into \code{BAT} package arguments.
-#'
-#' @param data A \code{matrix}, \code{data.frame} or \code{dispRity} object.
-#' @param subsets Optional, some specific subsets to extract (see \code{\link{get.subsets}}).
-#' @param matrix Optional, some specific matrices to extract (see \code{\link{get.matrix}}).
-#' @param tree Optional, some specific trees to extract (see \code{\link{get.tree}}).
-#' @param inc.all logical, whether to also add a group containing all elements (\code{TRUE}) or not (\code{FALSE}; default).
-#'
-#' @details
-#' Converts the content of a \code{dispRity} object into a list of arguments that can be used by \code{BAT} functions.
-#'
-#' @return
-#' \itemize{
-#'      \item \code{comm}: a \code{matrix} of presence/absence (\code{0, 1}) sorting the subsets by rows and the elements by columns that can be passed as the \code{comm} argument to \code{BAT} functions. If the \code{data} contains no subsets, the matrix is matrix full of 1 with one row and a number of columns corresponding to the number of elements in \code{data}. If \code{inc.all = TRUE} and \code{data} does not contain a subset with all data, an additional subset with all data is used.
-#'      \item \code{traits}: a \code{matrix} that is the traitspace with elements as rows and dimensions as columns.
-#'      \item \code{tree}: either \code{NULL} if the \code{data} contains no tree or a \code{phylo} or \code{multiPhylo} object from \code{data}.
-#' }
-#' 
-#' @examples
-#' ## Basic example
-#' data(demo_data)
-#' BAT_data <- dispRity.BAT(demo_data$jones)
-#' ## The community table
-#' BAT_data$comm
-#'
-#' ## Complex example
-#' data(disparity)
-#' ## The community table for complex data
-#' ## (including 100 bootstraps for 4 rarefaction levels for 7 subsets
-#' ## plus the 7 raw subsets for 99 elements = 2807*99)
-#' dim(dispRity.BAT(disparity)$comm)
-#'
-## Converts a dispRity object into BAT arguments
-dispRity.BAT <- function(data, subsets, matrix, tree, inc.all = FALSE) {
+# #' @title Converts \code{dispRity} to \code{BAT}.
+# #'
+# #' @description Converts a \code{dispRity} object into \code{BAT} package arguments.
+# #'
+# #' @param data A \code{matrix}, \code{data.frame} or \code{dispRity} object.
+# #' @param subsets Optional, some specific subsets to extract (see \code{\link{get.subsets}}).
+# #' @param matrix Optional, some specific matrices to extract (see \code{\link{get.matrix}}).
+# #' @param tree Optional, some specific trees to extract (see \code{\link{get.tree}}).
+# #' @param inc.all logical, whether to also add a group containing all elements (\code{TRUE}) or not (\code{FALSE}; default).
+# #'
+# #' @details
+# #' Converts the content of a \code{dispRity} object into a list of arguments that can be used by \code{BAT} functions.
+# #'
+# #' @return
+# #' \itemize{
+# #'      \item \code{comm}: a \code{matrix} of presence/absence (\code{0, 1}) sorting the subsets by rows and the elements by columns that can be passed as the \code{comm} argument to \code{BAT} functions. If the \code{data} contains no subsets, the matrix is matrix full of 1 with one row and a number of columns corresponding to the number of elements in \code{data}. If \code{inc.all = TRUE} and \code{data} does not contain a subset with all data, an additional subset with all data is used.
+# #'      \item \code{traits}: a \code{matrix} that is the traitspace with elements as rows and dimensions as columns.
+# #'      \item \code{tree}: either \code{NULL} if the \code{data} contains no tree or a \code{phylo} or \code{multiPhylo} object from \code{data}.
+# #' }
+# #' 
+# #' @examples
+# #' ## Basic example
+# #' data(demo_data)
+# #' BAT_data <- dispRity.BAT(demo_data$jones)
+# #' ## The community table
+# #' BAT_data$comm
+# #'
+# #' ## Complex example
+# #' data(disparity)
+# #' ## The community table for complex data
+# #' ## (including 100 bootstraps for 4 rarefaction levels for 7 subsets
+# #' ## plus the 7 raw subsets for 99 elements = 2807*99)
+# #' dim(dispRity.BAT(disparity)$comm)
+# #'
+# ## Converts a dispRity object into BAT arguments
+# dispRity.BAT <- function(data, subsets, matrix, tree, inc.all = FALSE) {
 
-    ## Check if data is dispRity
-    check.class(data, "dispRity")
-    ## Check if is subseted
-    is_subseted <- length(data$subsets) != 0
-    ## If so check if probabilistic
-    is_proba <- FALSE
-    if(is_subseted) {
-        is_proba <- !is.na(data$call$subsets[2]) && grepl("split", data$call$subsets[2])
-    }
+#     ## Check if data is dispRity
+#     check.class(data, "dispRity")
+#     ## Check if is subseted
+#     is_subseted <- length(data$subsets) != 0
+#     ## If so check if probabilistic
+#     is_proba <- FALSE
+#     if(is_subseted) {
+#         is_proba <- !is.na(data$call$subsets[2]) && grepl("split", data$call$subsets[2])
+#     }
 
-    ## Placeholders
-    comm <- tree <- traits <- NULL
+#     ## Placeholders
+#     comm <- tree <- traits <- NULL
 
-    ## Check if matrix exists
-    if(!missing(matrix)) {
-        matrix <- get.matrix(data, matrix = matrix)        
-    } else {
-        matrix <- data$matrix[[1]]
-    }
+#     ## Check if matrix exists
+#     if(!missing(matrix)) {
+#         matrix <- get.matrix(data, matrix = matrix)        
+#     } else {
+#         matrix <- data$matrix[[1]]
+#     }
 
-    ## Check if the data has subsets
-    if(is_subseted) {
-        ## Use subsets if not missing
-        if(!missing(subsets)) {
-            data <- get.subsets(data, subsets)
-        }
+#     ## Check if the data has subsets
+#     if(is_subseted) {
+#         ## Use subsets if not missing
+#         if(!missing(subsets)) {
+#             data <- get.subsets(data, subsets)
+#         }
        
-        ## Populate the comms table
-        comm <- do.call(rbind, unlist(lapply(data$subsets, lapply, make.a.subset.comm, matrix = matrix, is_proba = is_proba), recursive = FALSE))
+#         ## Populate the comms table
+#         comm <- do.call(rbind, unlist(lapply(data$subsets, lapply, make.a.subset.comm, matrix = matrix, is_proba = is_proba), recursive = FALSE))
 
-        ## Get the table elements
-        subset_names <- name.subsets(data)
+#         ## Get the table elements
+#         subset_names <- name.subsets(data)
         
-        ## If table had only elements name them as the subsets
-        if(nrow(comm) == length(subset_names)) {
-            rownames(comm) <- subset_names
-        } else {
-            ## Specify the bootstraps and rarefactions
-            names <- lapply(data$subsets, lapply, get.comm.names, matrix = matrix)
-            rownames <- character()
-            for(one_group in 1:length(names)) {
-               rownames <- c(rownames, paste0(subset_names[one_group], ".", unname(unlist(names[[one_group]]))))
-            }
-            rownames(comm) <- rownames
-        }
-        ## Inc all if no entire subset is in the data
-        if(inc.all && !any(apply(comm, 1, function(x) all(x == 1)))) {
-            comm <- rbind(comm, matrix(1, nrow = 1, ncol = nrow(matrix)))
-            rownames(comm)[nrow(comm)] <- "all"
-        }
-    } else {
-        ## No subsets
-        comm <- matrix(1, nrow = 1, ncol = nrow(matrix))
-    }
-    colnames(comm) <- rownames(matrix)
+#         ## If table had only elements name them as the subsets
+#         if(nrow(comm) == length(subset_names)) {
+#             rownames(comm) <- subset_names
+#         } else {
+#             ## Specify the bootstraps and rarefactions
+#             names <- lapply(data$subsets, lapply, get.comm.names, matrix = matrix)
+#             rownames <- character()
+#             for(one_group in 1:length(names)) {
+#                rownames <- c(rownames, paste0(subset_names[one_group], ".", unname(unlist(names[[one_group]]))))
+#             }
+#             rownames(comm) <- rownames
+#         }
+#         ## Inc all if no entire subset is in the data
+#         if(inc.all && !any(apply(comm, 1, function(x) all(x == 1)))) {
+#             comm <- rbind(comm, matrix(1, nrow = 1, ncol = nrow(matrix)))
+#             rownames(comm)[nrow(comm)] <- "all"
+#         }
+#     } else {
+#         ## No subsets
+#         comm <- matrix(1, nrow = 1, ncol = nrow(matrix))
+#     }
+#     colnames(comm) <- rownames(matrix)
 
-    ## Check if the tree exist
-    if(!is.null(data$tree[[1]])) {
-        if(!missing(tree)) {
-            if(missing(subsets)) {
-                subsets <- FALSE
-            }
-            ## Get the tree
-            tree <- get.tree(data, subsets = subsets)
-            ## If the tree is multiPhylo get the first one + warning
-            if(is(tree, "multiPhylo")) {
-                tree <- tree[[1]]
-                warning("data contained a distribution of tree. Only the first tree is used.")
-            }
-        }
-    }
+#     ## Check if the tree exist
+#     if(!is.null(data$tree[[1]])) {
+#         if(!missing(tree)) {
+#             if(missing(subsets)) {
+#                 subsets <- FALSE
+#             }
+#             ## Get the tree
+#             tree <- get.tree(data, subsets = subsets)
+#             ## If the tree is multiPhylo get the first one + warning
+#             if(is(tree, "multiPhylo")) {
+#                 tree <- tree[[1]]
+#                 warning("data contained a distribution of tree. Only the first tree is used.")
+#             }
+#         }
+#     }
 
-    ## Return everything
-    return(list(comm   = comm,
-                tree   = tree,
-                traits = matrix))
-}
+#     ## Return everything
+#     return(list(comm   = comm,
+#                 tree   = tree,
+#                 traits = matrix))
+# }
 
-## Transforms the trait matrix into a community one
-make.BAT.comm <- function(matrix, data) {
-    if(missing(data)) {
-        return(matrix(1, nrow = 1, ncol = nrow(matrix)))
-    } else {
-        return(matrix(as.integer(rownames(data) %in% rownames(matrix)), nrow = 1, ncol = nrow(data)))
-    }
-}
+# ## Transforms the trait matrix into a community one
+# make.BAT.comm <- function(matrix, data) {
+#     if(missing(data)) {
+#         return(matrix(1, nrow = 1, ncol = nrow(matrix)))
+#     } else {
+#         return(matrix(as.integer(rownames(data) %in% rownames(matrix)), nrow = 1, ncol = nrow(data)))
+#     }
+# }
 
-## Collapse a bootstrap probability
-collapse.proba <- function(proba_table) {
-    ## Empty collapse table
-    collapsed_matrix <- matrix(nrow = nrow(proba_table), ncol = 0)
-    ## Collapse the probabilities
-    while(ncol(proba_table) >= 3 && ((ncol(proba_table) %% 3) == 0)) {
-        sub_table <- proba_table[, 1:3]
-        collapsed_matrix <- cbind(collapsed_matrix, apply(sub_table, 1, function(x) sample(x[c(1,2)], size = 1, prob = c(x[3], 1-x[3]))))
-        proba_table <- proba_table[, -c(1:3), drop = FALSE]
-    }
-    return(collapsed_matrix)
-}
+# ## Collapse a bootstrap probability
+# collapse.proba <- function(proba_table) {
+#     ## Empty collapse table
+#     collapsed_matrix <- matrix(nrow = nrow(proba_table), ncol = 0)
+#     ## Collapse the probabilities
+#     while(ncol(proba_table) >= 3 && ((ncol(proba_table) %% 3) == 0)) {
+#         sub_table <- proba_table[, 1:3]
+#         collapsed_matrix <- cbind(collapsed_matrix, apply(sub_table, 1, function(x) sample(x[c(1,2)], size = 1, prob = c(x[3], 1-x[3]))))
+#         proba_table <- proba_table[, -c(1:3), drop = FALSE]
+#     }
+#     return(collapsed_matrix)
+# }
 
-## Make a comm row out of a subset
-make.a.subset.comm <- function(one_subset, matrix, is_proba = FALSE) {
-    ## Get the elements
-    if(is_proba) {
-        elements <- collapse.proba(one_subset)
-    } else {
-        elements <- one_subset[,, drop = FALSE]
-    }
+# ## Make a comm row out of a subset
+# make.a.subset.comm <- function(one_subset, matrix, is_proba = FALSE) {
+#     ## Get the elements
+#     if(is_proba) {
+#         elements <- collapse.proba(one_subset)
+#     } else {
+#         elements <- one_subset[,, drop = FALSE]
+#     }
 
-    ## Empty community matrix
-    comm_subset <- matrix(0, ncol = ncol(elements), nrow = nrow(matrix))
+#     ## Empty community matrix
+#     comm_subset <- matrix(0, ncol = ncol(elements), nrow = nrow(matrix))
 
-    ## Fill the community matrix
-    for(i in 1:ncol(comm_subset)) {
-        comm_subset[elements[,i],i] <- 1
-    } 
-    return(t(comm_subset))
-}
-## Get the comm names (bootstraps or rarefactions)
-get.comm.names <- function(one_subset, matrix) {
-    if(ncol(one_subset) == 1) {
-        return("elements")
-    } else {
-        return(paste0("bootstrap.", nrow(one_subset), ".", 1:ncol(one_subset)))
-    }
-}
+#     ## Fill the community matrix
+#     for(i in 1:ncol(comm_subset)) {
+#         comm_subset[elements[,i],i] <- 1
+#     } 
+#     return(t(comm_subset))
+# }
+# ## Get the comm names (bootstraps or rarefactions)
+# get.comm.names <- function(one_subset, matrix) {
+#     if(ncol(one_subset) == 1) {
+#         return("elements")
+#     } else {
+#         return(paste0("bootstrap.", nrow(one_subset), ".", 1:ncol(one_subset)))
+#     }
+# }
 
--- tests/testthat/test-dispRity.BAT.R
+++ tests/testthat/test-dispRity.BAT.R
@@ -1,71 +1,71 @@
-test_that("make.BAT.comm works", {
-    dummy_matrix <- matrix(rnorm(90), 10, 9, dimnames = list(letters[1:10]))
-    sub_matrix <- dummy_matrix[c(1:3), ]
-    test <- make.BAT.comm(sub_matrix)
-    expect_equal(dim(test), c(1,3))
-    expect_equal(sum(test), 3)
-    test <- make.BAT.comm(sub_matrix, data = dummy_matrix)
-    expect_equal(dim(test), c(1,10))
-    expect_equal(sum(test), 3)
-})
+# test_that("make.BAT.comm works", {
+#     dummy_matrix <- matrix(rnorm(90), 10, 9, dimnames = list(letters[1:10]))
+#     sub_matrix <- dummy_matrix[c(1:3), ]
+#     test <- make.BAT.comm(sub_matrix)
+#     expect_equal(dim(test), c(1,3))
+#     expect_equal(sum(test), 3)
+#     test <- make.BAT.comm(sub_matrix, data = dummy_matrix)
+#     expect_equal(dim(test), c(1,10))
+#     expect_equal(sum(test), 3)
+# })
 
-test_that("dispRity.BAT works", {
-    ## Test convert simple
-    dummy_matrix <- matrix(rnorm(90), 10, 9, dimnames = list(letters[1:10]))
-    data <- make.dispRity(dummy_matrix)
-    out <- dispRity.BAT(data)
-    expect_is(out, "list")
-    expect_equal(names(out), c("comm", "tree", "traits"))
-    expect_equal(dim(out$traits), c(10,9))
-    expect_equal(dim(out$comm), c(1,10))
-    expect_true(is.null(out$tree))
+# test_that("dispRity.BAT works", {
+#     ## Test convert simple
+#     dummy_matrix <- matrix(rnorm(90), 10, 9, dimnames = list(letters[1:10]))
+#     data <- make.dispRity(dummy_matrix)
+#     out <- dispRity.BAT(data)
+#     expect_is(out, "list")
+#     expect_equal(names(out), c("comm", "tree", "traits"))
+#     expect_equal(dim(out$traits), c(10,9))
+#     expect_equal(dim(out$comm), c(1,10))
+#     expect_true(is.null(out$tree))
 
-    ## Test convert complex
-    data(demo_data)
-    eco_data <- demo_data$jones    
-    out <- dispRity.BAT(eco_data)
-    expect_is(out, "list")
-    expect_equal(names(out), c("comm", "tree", "traits"))
-    expect_equal(dim(out$traits), c(48,47))
-    expect_equal(dim(out$comm), c(2,48))
-    expect_true(is.null(out$tree))
+#     ## Test convert complex
+#     data(demo_data)
+#     eco_data <- demo_data$jones    
+#     out <- dispRity.BAT(eco_data)
+#     expect_is(out, "list")
+#     expect_equal(names(out), c("comm", "tree", "traits"))
+#     expect_equal(dim(out$traits), c(48,47))
+#     expect_equal(dim(out$comm), c(2,48))
+#     expect_true(is.null(out$tree))
 
-    out <- dispRity.BAT(eco_data, inc.all = TRUE)
-    expect_equal(dim(out$traits), c(48,47))
-    expect_equal(dim(out$comm), c(3,48))
-    expect_true(is.null(out$tree))
+#     out <- dispRity.BAT(eco_data, inc.all = TRUE)
+#     expect_equal(dim(out$traits), c(48,47))
+#     expect_equal(dim(out$comm), c(3,48))
+#     expect_true(is.null(out$tree))
 
-    ## Test convert complex
-    data(disparity)
-    out <- dispRity.BAT(disparity)
-    expect_is(out, "list")
-    expect_equal(names(out), c("comm", "tree", "traits"))
-    expect_equal(dim(out$traits), c(99,97))
-    expect_equal(dim(out$comm), c(2807,99))
-    expect_is(out$tree, "phylo")
-})
+#     ## Test convert complex
+#     data(disparity)
+#     out <- dispRity.BAT(disparity)
+#     expect_is(out, "list")
+#     expect_equal(names(out), c("comm", "tree", "traits"))
+#     expect_equal(dim(out$traits), c(99,97))
+#     expect_equal(dim(out$comm), c(2807,99))
+#     expect_is(out$tree, "phylo")
+# })
 
 
-test_that("dispRity.BAT works with bootstraps", {
-    ## Test convert simple
-    data(demo_data)
-    data <- boot.matrix(demo_data$jones, bootstraps = 3, rarefaction = c(24,12))
-    out <- dispRity.BAT(data)
-    expect_equal(names(out), c("comm", "tree", "traits"))
-    expect_equal(dim(out$traits), c(48,47))
-    expect_equal(dim(out$comm), c(14,48))
-    expect_equal(rownames(out$comm)[c(1,7,2,8,6,14)], c("aspen.elements", "aspen.bootstrap.12.3", "aspen.bootstrap.24.1", "grassland.elements", "aspen.bootstrap.12.2", "grassland.bootstrap.12.3"))
-    expect_true(is.null(out$tree))
+# test_that("dispRity.BAT works with bootstraps", {
+#     ## Test convert simple
+#     data(demo_data)
+#     data <- boot.matrix(demo_data$jones, bootstraps = 3, rarefaction = c(24,12))
+#     out <- dispRity.BAT(data)
+#     expect_equal(names(out), c("comm", "tree", "traits"))
+#     expect_equal(dim(out$traits), c(48,47))
+#     expect_equal(dim(out$comm), c(14,48))
+#     expect_equal(rownames(out$comm)[c(1,7,2,8,6,14)], c("aspen.elements", "aspen.bootstrap.12.3", "aspen.bootstrap.24.1", "grassland.elements", "aspen.bootstrap.12.2", "grassland.bootstrap.12.3"))
+#     expect_true(is.null(out$tree))
 
-    ## Works with probabilistic bootstraps
-    data(BeckLee_mat99)
-    data(BeckLee_ages)
-    data(BeckLee_tree)
-    data <- chrono.subsets(BeckLee_mat99, BeckLee_tree, method = "continuous", time = c(120, 100, 80, 60, 40 , 20, 0), model = "gradual.split", inc.nodes = TRUE, FADLAD = BeckLee_ages, verbose = FALSE, t0 = FALSE)
-    out <- dispRity.BAT(data)
-    expect_equal(names(out), c("comm", "tree", "traits"))
-    expect_equal(dim(out$traits), c(99,97))
-    expect_equal(dim(out$comm), c(7,99))
-    expect_equal(rownames(out$comm), as.character(seq(from = 120, to = 0, by = -20)))
-    expect_equal(unique(c(out$comm)), c(0,1))
-})
+#     ## Works with probabilistic bootstraps
+#     data(BeckLee_mat99)
+#     data(BeckLee_ages)
+#     data(BeckLee_tree)
+#     data <- chrono.subsets(BeckLee_mat99, BeckLee_tree, method = "continuous", time = c(120, 100, 80, 60, 40 , 20, 0), model = "gradual.split", inc.nodes = TRUE, FADLAD = BeckLee_ages, verbose = FALSE, t0 = FALSE)
+#     out <- dispRity.BAT(data)
+#     expect_equal(names(out), c("comm", "tree", "traits"))
+#     expect_equal(dim(out$traits), c(99,97))
+#     expect_equal(dim(out$comm), c(7,99))
+#     expect_equal(rownames(out$comm), as.character(seq(from = 120, to = 0, by = -20)))
+#     expect_equal(unique(c(out$comm)), c(0,1))
+# })
