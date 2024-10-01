f15ce32334b78f79fcf394e21ae8709cbee7a933
Thomas Guillerme <guillert@tcd.ie>
2024-10-01 14:29:38 +0100

    pre test generalise

--- tests/testthat/test-dist.helper.R
+++ tests/testthat/test-dist.helper.R
@@ -155,55 +155,115 @@ test_that("works with bootstraps", {
     check.class(test, "dispRity")
     expect_equal(dim(summary(test)), c(10, 8))
     expect_equal(summary(test)$obs.median, c(2.472, 2.537, 2.623, 2.723, 2.750, 2.785, 2.841, 2.867, 2.867, 2.867))
-
-    ## CHECK IF IT ACTUALLY DOES RECYCLE THE dist.helper!!!!
-
-    # test <- microbenchmark("with pre-clac"= dispRity(data = bs_data, metric = pairwise.dist, dist.helper = dist_matrix),
-    #                        "with help"    = dispRity(data = bs_data, metric = pairwise.dist, dist.helper = dist),
-    #                        "without help" = dispRity(data = bs_data, metric = pairwise.dist))
-    # plot(test, main = "10 matrices and 100 BS", ylab = "milliseconds", xlab = "")
 })
-
-## Show differences
-
-# set.seed(1)
-# data <- matrix(rnorm(20000), 200, 100, dimnames = list(as.character(1:200)))
-# dist_matrix <- vegan::vegdist(data, method = "euclidean")
-
-
-
-pairwise.dist
-
-
-
-# library(microbenchmark)
-# test <- microbenchmark("with pre-clac"= dispRity(data = data, metric = pairwise.dist, dist.helper = dist_matrix),
-#                        "with help"    = dispRity(data = data, metric = pairwise.dist, dist.helper = vegan::vegdist),
-#                        "without help" = dispRity(data = data, metric = pairwise.dist))
-# plot(test, main = "One matrix", ylab = "milliseconds", xlab = "")
-
-
 
 
 test_that("works with trees", {
-    
+    # set.seed(1)
+    # tree <- read.tree(text = "((((((A,B), C), D), E), F), G);")
+    # tree$edge.length <- rep(1, 7+6)
+    # tree$node.label <- letters[1:6]
+    # ## An empty matrix (with the right elements)
+    # matrix <- matrix(rnorm((7+6)*2), nrow = 7+6, ncol = 2)
+    # rownames(matrix) <- c(tree$tip.label, tree$node.label)
+
+    # ## Simple test
+    # test <- dispRity(data = matrix, metric = edge.length.tree, tree = tree)
+    # expect_equal(c(test$disparity[[1]][[1]]), edge.length.tree(matrix, tree))
+
+    # ## More complex metric test
+    # test <- dispRity(data = matrix, metric = projections.tree, tree = tree, type = c("root","ancestor"))
+    # expect_equal(c(test$disparity[[1]][[1]]), projections.tree(matrix, tree))
 })
 
 test_that("works with covar", {
     
+    # data(charadriiformes)
+    # data <- MCMCglmm.subsets(data       = charadriiformes$data,
+    #                          posteriors = charadriiformes$posteriors,
+    #                          tree       = charadriiformes$tree,
+    #                          group      = MCMCglmm.levels(
+    #                                  charadriiformes$posteriors)[1:3],
+    #                          rename.groups = c("gulls", "plovers", "sandpipers"))
+
+    # ## Test works in 2 times (1st covar)
+    # testA <- dispRity(data, metric = as.covar(variances), dimensions = c(1:17))
+    # expect_is(testA, "dispRity")
+    # expect_equal(names(testA), c("matrix", "tree", "call", "subsets", "covar", "disparity"))
+    # expect_equal(c(summary(testA)$`97.5%`), c(0.068, 0.002, 0.016))
 })
 
 test_that("works with between groups", {
+    # between.groups.simple <- function(matrix, matrix2) return(42)
+    # between.groups.complex <- function(matrix, matrix2) return(mean(matrix) - mean(matrix2))
+
+    # ## Testing data
+    # matrix <- do.call(rbind, list(matrix(1, 5, 5), matrix(2, 3, 5), matrix(3, 4, 5)))
+    # matrix_node <- do.call(rbind, list(matrix(3, 4, 5), matrix(2, 3, 5), matrix(1, 4, 5)))
+    # rownames(matrix) <- paste0("t", 1:12)
+    # test_tree <- stree(12, type = "right")
+    # rownames(matrix_node) <- paste0("n", 1:Nnode(test_tree))
+    # matrix_node <- rbind(matrix, matrix_node)
+    # test_tree$node.label <- paste0("n", 1:Nnode(test_tree))
+    # test_tree$edge.length <- rep(1, Nedge(test_tree))
+    # test_tree$root.time <- 12
+
+    # ## custom subsets
+    # custom <- custom.subsets(matrix, group = list(c(1:5), c(6:8), c(9:12)))
+    # chrono <- chrono.subsets(matrix, test_tree, method = "discrete", time = c(12, 8.1, 5.1, 0))
+
+
+    # ## Custom normal
+    # test <- dispRity(custom, metric = between.groups.complex, between.groups = TRUE)
+    # expect_equal(capture.output(test)[4], "Disparity was calculated as: between.groups.complex between groups.")
+    # summary_results <- summary(test)
+    # expect_is(summary_results, "data.frame")
+    # expect_equal(dim(summary_results), c(3, 4))
+    # expect_equal(colnames(summary_results), c("subsets", "n_1", "n_2", "obs"))
+    # expect_equal(summary_results$subsets, c("1:2", "1:3", "2:3"))
+    # expect_equal(summary_results$obs, c(-1, -2, -1))
+    # expect_null(plot(test))
+
+
 
 })
 
 test_that("works with multiple matrices", {
 
+    # ## Works with level 2 and subsets
+    # matrices_list <- space.maker(elements = 30, dimensions = 5, distribution = rnorm, replicates = 3)
+    # ## warning is added rownames
+    # expect_warning(matrices_groups <- custom.subsets(data = matrices_list, group = list("group1" = 1:20, "group2" = 21:30)))
+    # data <- dispRity(data = matrices_groups, metric = centroids)
+    # expect_equal(dim(data$disparity[[1]]$elements), c(20, 3))
+    # expect_equal(dim(data$disparity[[2]]$elements), c(10, 3))
 })
 
-
-
 test_that("works with dispRity multi", {
+    # ## Two matrices and two trees
+    # set.seed(1)
+    # tree <- rmtree(2, 10)
+    # tree[[1]] <- makeNodeLabel(tree[[1]])
+    # tree[[2]] <- makeNodeLabel(tree[[2]], prefix = "shnode")
+    # tree[[1]]$root.time <- max(tree.age(tree[[1]])$ages)
+    # tree[[2]]$root.time <- max(tree.age(tree[[2]])$ages)
+
+    # data <- list(matrix(0, nrow = Ntip(tree[[1]]) + Nnode(tree[[1]]), dimnames = list(c(tree[[1]]$tip.label, tree[[1]]$node.label))),
+    #              matrix(0, nrow = Ntip(tree[[2]]) + Nnode(tree[[2]]), dimnames = list(c(tree[[2]]$tip.label, tree[[2]]$node.label))))
+
+    # ## Test working fine
+    # expect_warning(chrono_subsets <- chrono.subsets(data = data, tree = tree, time = 3, method = "continuous", model = "acctran"))
+    # expect_warning(test <- dispRity(chrono_subsets, metric = centroids))
+    # expect_is(test, c("dispRity", "multi"))
+    # expect_equal(names(test), c("matrix", "tree", "call", "subsets", "disparity"))
+    # expect_equal(capture.output(test), c(
+    #     " ---- dispRity object ---- ",
+    #     "3 continuous (acctran) time subsets for 19 elements in 2 separated matrices with 2 phylogenetic trees",
+    #     "    2.62/1.95, 1.31/0.98, 0.",
+    #     "Disparity was calculated as: centroids." 
+    # ))
+    # expect_null(plot(test))
+    # expect_equal(summary(test)$obs.median, c(0, 0, NA))
 
 })
 
