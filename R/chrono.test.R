b85637fa1cf6b70f960c876571e290daef0b0fbc
Thomas Guillerme <guillert@tcd.ie>
2026-06-02 14:24:36 +0100

    sanitizing

--- R/chrono.test.R
+++ R/chrono.test.R
@@ -43,24 +43,53 @@
 
 
 chrono.test <- function(data, method, changepoint, time.window, ...) {
+    
     match_call <- match.call()
-    ##############
-    # SANITISING #
-    ##############
+    
+    ## Check the data
+    is_data_error <- FALSE
+    if(!is(data, "dispRity")) {
+        is_data_error <- TRUE
+    }
+    if(!is.null(data$subsets) && !is.null(data$call)) {
+        if(disparity$call$subsets[[1]] != "continuous") {
+            is_data_error <- TRUE
+            stop.call(call = NULL, "chrono.test is not implemented yes for customised subsets")
+        }
+    }
+    if(is.null(data$disparity)) {
+        is_data_error <- TRUE
+    }
+    if(is_data_error) {
+        stop.call(call = match_call$data, msg = " must be a dispRity object with time series and disparity data.")
+    }
 
-    ## start with data
-    check.class(data, "dispRity") ## do all the classic dispRity checking, is_multi etc, make sure it has a tree and a matrix.
-    mat <- get.matrix(data)
-    tree <- get.tree(data)
+    ## Check and set up the changepoint and time.window
+    changepoint_class <- check.class(changepoint, c("numeric", "integer", "character"))
+    if(changepoint_class == "character") {
+        if(changepoint_class != "detect") {
+            stop.call(call = NULL, msg = "changepoint argument must be a numeric or integer vector or \"detect\" to automatically detect the changepoint.")
+        }
+    }
+    check.class(time.window, c("numeric", "integer"))
 
+    ## Time checks
+    changepoint <- check.time(changepoint, type = "changepoint", data = data)
+    time.window <- check.time(time.window, type = "time.window", data = data)
 
+    ## Check the method
+    method_type <- check.class(method, c("character", "function"))
 
-    check.time(changepoint, c("numeric", "integer", ))
-    check.length(changepoint, 1, " must be a single numeric value.")
-    # check.class(replicates, c("numeric"))
-    # check.length(replicates, 1, " must be a single numeric value.")
-    methods <- c("itsa", "citsa", "area", "h.test")
-    check.method(slice.model, slice_models, "slice.model argument")
+    if(method_type == "character") {
+        ## Standard methods
+        chrono_test_methods <- c("itsa", "citsa", "area", "average")
+        method_out <- check.method(method, chrono_test_methods, "method argument")
+    } else {
+        ## Check if method is a h.test function
+        method <- "average"
+        stop.call(call = NULL, msg = "user function for method not implemented yet.")
+    }
+
 
 
 
@@ -68,11 +97,11 @@ chrono.test <- function(data, method, changepoint, time.window, ...) {
 
     
 
-    delta_df <- make.deltatronic(data, changepoint)
+    # delta_df <- make.deltatronic(data, changepoint)
 
-    if(!is.null(time.window)) {
-        delta_df <- set.time.window(delta_df, time.window, changepoint)
-    }
+    # if(!is.null(time.window)) {
+    #     delta_df <- set.time.window(delta_df, time.window, changepoint)
+    # }
 
 
     
@@ -80,8 +109,7 @@ chrono.test <- function(data, method, changepoint, time.window, ...) {
 
 
 
-
+    return(NULL)
 
 }
 
-## sanitizing function for changepoint + time.window
