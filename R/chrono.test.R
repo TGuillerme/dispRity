073c5ef20f34acc760d5bd0821403dbcd0797985
Thomas Guillerme <guillert@tcd.ie>
2026-06-02 15:18:20 +0100

    sanitizing for chrono.test

--- R/chrono.test.R
+++ R/chrono.test.R
@@ -4,8 +4,8 @@
 #'
 #' @param data a \code{"dispRity"} object with time series (e.g. from \code{chrono.subsets}) and disparity values
 #' @param method the method for measuring change (either \code{"itsa"}, \code{"citsa"}, \code{"area"}, \code{"average"} or any generic \code{h.test} function). See details.
-#' @param changepoint
-#' @param time.window
+#' @param changepoint one or more \code{"numeric"} or \code{"integer"} values of where to measure the changes in disparity time series. This can be set to \code{"detect"} to test all the time subsets available.
+#' @param time.window optional, either two \code{"numeric"} or \code{"integer"} values to narrow than the time window to perform the test on or a single value that can be either a proportion of subsets to use before and after the \code{changepoint} (up to \code{0.5}) or a number of subsets to use before and after the \code{changepoint}.
 #' @param ... A named list of specific options for each different method. See details.
 #' 
 #' @details
@@ -47,24 +47,20 @@ chrono.test <- function(data, method, changepoint, time.window, ...) {
     match_call <- match.call()
     
     ## Check the data
-    is_data_error <- FALSE
+    error_disparity <- " must be a dispRity object with a tree, time series and disparity data."
     if(!is(data, "dispRity")) {
-        is_data_error <- TRUE
+       stop.call(call = match_call$data, msg = error_disparity)
     }
     if(!is.null(data$subsets) && !is.null(data$call)) {
-        if(disparity$call$subsets[[1]] != "continuous") {
-            is_data_error <- TRUE
-            stop.call(call = NULL, "chrono.test is not implemented yes for customised subsets")
+        if(data$call$subsets[[1]] != "continuous") {
+            stop.call(msg = "chrono.test is not implemented yes for customised subsets")
         }
     }
     if(!is(data$tree[[1]], "phylo")) {
-        is_data_error <- TRUE
+        stop.call(call = match_call$data, msg = error_disparity)
     }
     if(is.null(data$disparity)) {
-        is_data_error <- TRUE
-    }
-    if(is_data_error) {
-        stop.call(call = match_call$data, msg = " must be a dispRity object with a tree, time series and disparity data.")
+        stop.call(call = match_call$data, msg = error_disparity)
     }
 
     ## Check and set up the changepoint and time.window
@@ -72,23 +68,28 @@ chrono.test <- function(data, method, changepoint, time.window, ...) {
     changepoint_class <- check.class(changepoint, c("numeric", "integer", "character"))
     if(changepoint_class == "character") {
         if(changepoint_class != "detect") {
-            stop.call(call = NULL, msg = "changepoint argument must be a numeric or integer vector or \"detect\" to automatically detect the changepoint.")
+            stop.call(msg = "changepoint argument must be a numeric or integer vector or \"detect\" to automatically detect the changepoint.")
         }
     } else {
         check.time.range <- function(x, all_time_range, name) {
             if(any(x < min(all_time_range)) || any(x > max(all_time_range))) {
-                stop.call(msg.pre = paste0(name, add.s(x)), call = NULL, msg = paste0(" falls out of the time range of the data tree", add.s(data$tree)," (", min(all_time_range), " - ", max(all_time_range), ")."))
+                stop.call(msg.pre = paste0(name, add.s(x)), msg = paste0(" falls out of the time range of the data tree", add.s(data$tree)," (", min(all_time_range), " - ", max(all_time_range), ")."))
             }
         }
         check.time.range(changepoint, all_time_range, name = "changepoint")
     }
-    check.class(time.window, c("numeric", "integer"))
-    if(length(time.window) < 3) {
-        if(length(time.window) == 2) {
-            check.time.range(time.window, all_time_range, name = "time.window")
-        }
+
+    if(missing(time.window)) {
+        time.window <- all_time_range
     } else {
-        stop.call(call = NULL, msg = "time.window can only contain 2 values (the time ranges) or 1 value (the proportion or number of subsets before and after the time).")
+        check.class(time.window, c("numeric", "integer"))
+        if(length(time.window) < 3) {
+            if(length(time.window) == 2) {
+                check.time.range(time.window, all_time_range, name = "time.window")
+            }
+        } else {
+            stop.call(msg = "time.window can only contain 2 values (the time ranges) or 1 value (the proportion or number of subsets before and after the time).")
+        }
     }
 
     ## Check the method
@@ -101,7 +102,7 @@ chrono.test <- function(data, method, changepoint, time.window, ...) {
     } else {
         ## Check if method is a h.test function
         method <- "average"
-        stop.call(call = NULL, msg = "user function for method not implemented yet.")
+        stop.call(msg = "user function for method not implemented yet.")
     }
 
 
--- tests/testthat/test-chrono.test.R
+++ tests/testthat/test-chrono.test.R
@@ -6,5 +6,34 @@ mat <- matrix(rnorm(995), 199, 5)
 rownames(mat) <- c(tree$tip.label, tree$node.label)
 data <- make.dispRity(data = mat, tree = tree)
 data <- chrono.subsets(data, method = "c", model = "equal.split", time = 10, inc.nodes = TRUE)
+## Warning is for the last time slice that's 0
+expect_warning(data <- dispRity(data, metric = mean))
 
-data <- dispRity(data, metric = mean)
+test_that("sanitizing works", {
+	data(disparity)
+	wrong_data <- dispRity(custom.subsets(make.dispRity(data = mat, tree = tree), group = tree), metric = mean)
+
+	error <- capture_error(chrono.test("disparity", method = "average", changepoint = 66))
+	expect_equal(error[[1]], "disparity must be a dispRity object with a tree, time series and disparity data.")
+	error <- capture_error(chrono.test(wrong_data, method = "average", changepoint = 66))
+	expect_equal(error[[1]], "chrono.test is not implemented yes for customised subsets")
+	error <- capture_error(chrono.test(remove.tree(disparity), method = "average", changepoint = 66))
+	expect_equal(error[[1]], "remove.tree(disparity) must be a dispRity object with a tree, time series and disparity data.")
+	wrong_data <- disparity
+	wrong_data$disparity <- NULL
+	error <- capture_error(chrono.test(wrong_data, method = "average", changepoint = 66))
+	expect_equal(error[[1]], "wrong_data must be a dispRity object with a tree, time series and disparity data.")
+
+	error <- capture_error(chrono.test(disparity, method = "average", changepoint = 140))
+	expect_equal(error[[1]], "changepoint falls out of the time range of the data tree (0 - 139.0743).")
+	error <- capture_error(chrono.test(disparity, method = "average", changepoint = c(66, 140)))
+	expect_equal(error[[1]], "changepoints falls out of the time range of the data tree (0 - 139.0743).")
+
+	error <- capture_error(chrono.test(disparity, method = "average", changepoint = 66, time.window = c(0, 140)))
+	expect_equal(error[[1]], "time.windows falls out of the time range of the data tree (0 - 139.0743).")
+
+	error <- capture_error(chrono.test(disparity, method = "averagesss", changepoint = 66, time.window = 3))
+	expect_equal(error[[1]], "method argument must be one of the following: itsa, citsa, area, average.")
+	error <- capture_error(chrono.test(disparity, method = t.test, changepoint = 66, time.window = 3))
+	expect_equal(error[[1]], "user function for method not implemented yet.")
+})
