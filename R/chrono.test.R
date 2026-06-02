e14b876b5c1069e483bb1e0b06336c3e3a14ce30
Thomas Guillerme <guillert@tcd.ie>
2026-06-02 14:04:33 +0100

    header INIT

--- R/chrono.test.R
+++ R/chrono.test.R
@@ -1,3 +1,47 @@
+#' @title Disparity time series changes
+#'
+#' @description Check for changes in disparity time series
+#'
+#' @param data a \code{"dispRity"} object with time series (e.g. from \code{chrono.subsets}) and disparity values
+#' @param method the method for measuring change (either \code{"itsa"}, \code{"citsa"}, \code{"area"}, \code{"average"} or any generic \code{h.test} function). See details.
+#' @param changepoint
+#' @param time.window
+#' @param ... A named list of specific options for each different method. See details.
+#' 
+#' @details
+#' The implemented disparity time series changes are:
+#' \itemize{
+#'      \item \code{"itsa"}  @@@...
+#'      \item \code{"citsa"} @@@... 
+#'      \item \code{"area"}  @@@...
+#'      \item \code{"average"}  @@@...
+#' }
+#' 
+#' Each method can take the following specific arguments
+#' 
+#' \itemize{
+#'      \item \code{"itsa"}  @@@...
+#'      \item \code{"citsa"} @@@... 
+#'      \item \code{"area"}  @@@...
+#'      \item \code{"average"}  @@@...
+#' }
+#' 
+#' @returns
+#' 
+#' @@@
+#' 
+#' @examples
+#' 
+#' @@@
+#' 
+#' @seealso \code{\link{chrono.subsets}} \code{\link{dispRity}}
+#' 
+#' @author Caleb Scutt, Thomas Guillerme
+#' @references
+#' @@@
+#' 
+
+
 chrono.test <- function(data, method, changepoint, time.window, ...) {
     match_call <- match.call()
     ##############
